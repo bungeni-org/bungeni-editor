b4c81c28927d6af4b882fafadc882e553f40e755
ashok.hariharan <kohsah@users.noreply.github.com>
2009-01-21 15:39:40 +0000

    Adding URI generator project

---
+++ BungeniUriGenerator/src/org/bungeni/uri/BungeniURI.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.bungeni.uri;
+
+/**
+ *
+ * @author undesa
+ */
+public class BungeniURI {
+
+    /**
+     * URI components
+     */
+     CountryCode countryCode;
+     
+}
---
+++ BungeniUriGenerator/src/org/bungeni/uri/CountryCode.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.bungeni.uri;
+
+/**
+ *
+ * @author undesa
+ */
+public class CountryCode {
+  public String countryCode;
+        public String countryName;
+        
+        public CountryCode(){
+            countryCode = countryName = "";
+        }
+        public CountryCode(String countryC, String countryN) {
+            countryCode = countryC;
+            countryName = countryN;
+        }
+        
+        @Override
+        public String toString(){
+            return countryName;
+        }
+}
