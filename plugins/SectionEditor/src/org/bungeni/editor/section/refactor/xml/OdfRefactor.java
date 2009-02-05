9f04b33bf9da7022eaf8562f9ce8fc3c5d922fa4
ashok.hariharan <kohsah@users.noreply.github.com>
2009-02-05 15:40:08 +0000

    converted refactor to use JDom detach / addContent instead of Xerces insertBefore/after

--- plugins/SectionEditor/src/org/bungeni/editor/section/refactor/xml/OdfRefactor.java
+++ plugins/SectionEditor/src/org/bungeni/editor/section/refactor/xml/OdfRefactor.java
@@ -7,26 +7,16 @@ package org.bungeni.editor.section.refactor.xml;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.StringWriter;
-import java.util.Set;
+import java.util.Iterator;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.xml.transform.Result;
-import javax.xml.transform.Source;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
+import org.jdom.Attribute;
 import org.jdom.Element;
-import org.jdom.JDOMException;
 import org.jdom.input.SAXBuilder;
 import org.jdom.output.DOMOutputter;
 import org.jdom.output.XMLOutputter;
 import org.openoffice.odf.pkg.OdfPackage;
-import org.openoffice.odf.pkg.manifest.OdfFileEntry;
-import org.w3c.dom.NamedNodeMap;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 
 /**
  *
@@ -44,6 +34,7 @@ public class OdfRefactor {
     org.w3c.dom.Document domDocument;
     private static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(OdfPackage.class.getName());
 
+    private final static String ODF_CONTENT_FILE="content.xml";
     /**
      * Sets up the OdfRefactor object
      * @param odfFile - Path to ODF file
@@ -51,9 +42,9 @@ public class OdfRefactor {
     public OdfRefactor(String odfFile) {
         try {
             odfPackage = OdfPackage.loadPackage(odfFile);
-            Set<String> setOfFileEntries = odfPackage.getFileEntries();
-            OdfFileEntry fileEntry  = odfPackage.getFileEntry("content.xml");
-            InputStream is = odfPackage.getInputStream("content.xml");
+            //Set<String> setOfFileEntries = odfPackage.getFileEntries();
+            //OdfFileEntry fileEntry  = odfPackage.getFileEntry(ODF_CONTENT_FILE);
+            InputStream is = odfPackage.getInputStream(ODF_CONTENT_FILE);
             SAXBuilder saxBuilder = new SAXBuilder();
             xmlDocument = saxBuilder.build(is);
             DOMOutputter converter = new DOMOutputter();
@@ -63,7 +54,10 @@ public class OdfRefactor {
         }
     }
 
-    
+
+
+
+    /*
     private Node findSectionByName(String sectionName) {
         Node sourceSectionNode = null;
         NodeList sectionsList = domDocument.getElementsByTagName(ODF_SECTION_TAG);
@@ -79,9 +73,34 @@ public class OdfRefactor {
         }
         return sourceSectionNode;
     }
+    */
 
-    private static String ODF_SECTION_TAG = "text:section";
-    private static String ODF_SECTION_NAME_ATTRIBUTE = "text:name";
+
+    private Element getSectionByName(String lookForThisSection) {
+        Element matchingElement = null;
+        try {
+
+            ElementAttributeFilter eaf = new ElementAttributeFilter(SECTION_ELEMENT_WITHOUT_NS);
+            eaf.filterBy(SECTION_NAME_ATTR_WITHOUT_NS, lookForThisSection );
+            Iterator  sectionIter = xmlDocument.getRootElement().getDescendants(eaf) ;
+            while (sectionIter.hasNext()) {
+                matchingElement = (Element) sectionIter.next();
+                log.info("getSectionByName, found section " + lookForThisSection );
+                break;
+            }
+        } catch (Exception ex) {
+            log.error("getSectionByName : " + ex.getMessage());
+        } finally {
+            return matchingElement;
+        }
+     }
+
+
+    private static String SECTION_ELEMENT = "text:section";
+    private static String SECTION_NAME_ATTR = "text:name";
+    private static final String SECTION_ELEMENT_WITHOUT_NS = "section";
+    private static final String SECTION_NAME_ATTR_WITHOUT_NS = "name";
+
     /**
      * Refactors a source section and places it before the target section (the parent element remaining the same)
      * @param sourceSection
@@ -91,6 +110,43 @@ public class OdfRefactor {
     public boolean moveSectionBefore(String sourceSection, String targetSectionBefore) throws Exception {
         boolean bState = false;
         try {
+            //get the source element
+            Element elemSourceSection = this.getSectionByName(sourceSection);
+            if (elemSourceSection == null ) {
+                throw new Exception("Section sourceSection : " + sourceSection + " was not found");
+            }
+            //get the target element
+            Element elemTargetSection = this.getSectionByName(targetSectionBefore);
+            if (elemTargetSection == null ) {
+                throw new Exception("Section targetSectionNode : " + targetSectionBefore + " was not found");
+            }
+
+            //detach the source elements so that it can be refactored.
+            elemSourceSection.detach();
+
+            //Now refactor the source into the target element's parent
+            //getParent().getContent(), returns a "live" list
+            List contentList = elemTargetSection.getParent().getContent();
+            //iterate the live list 
+            for (int i=0; i < contentList.size(); i++) {
+                Object elemObject = contentList.get(i);
+                //check if the object found is an "Element"
+                if (elemObject.getClass().getName().equals(Element.class.getName())) {
+                    Element contentElem = (Element) elemObject;
+                    if (contentElem.getName().equals(SECTION_ELEMENT_WITHOUT_NS)) {
+                        Attribute nameAttr = contentElem.getAttribute(SECTION_NAME_ATTR_WITHOUT_NS, contentElem.getNamespace());
+                        if (nameAttr != null) {
+                            if (nameAttr.getValue().equals(targetSectionBefore)) {
+                                contentList.add(i, elemSourceSection);
+                                System.out.println("preamble found");
+                                break;
+                            }
+                        }
+                     }
+               }
+             }
+
+            /*
             DOMOutputter converter = new DOMOutputter();
             org.w3c.dom.Document domDoc = converter.output(xmlDocument);
             //get source section node
@@ -115,6 +171,8 @@ public class OdfRefactor {
             Transformer transformer = factory.newTransformer();
             transformer.transform(source, result);
             System.out.println(swriter.getBuffer().toString());
+             */
+
             bState = true;
         } catch (Exception ex) {
             log.error("moveSectionBefore : " + ex.getMessage());
