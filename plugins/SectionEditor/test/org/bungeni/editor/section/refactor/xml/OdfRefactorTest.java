36a65629c88af5edddc8cf1bbeec7bfa3af552a4
ashok.hariharan <kohsah@users.noreply.github.com>
2009-02-09 08:05:22 +0000

    unit test for section refactor

---
+++ plugins/SectionEditor/test/org/bungeni/editor/section/refactor/xml/OdfRefactorTest.java
@@ -0,0 +1,63 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.bungeni.editor.section.refactor.xml;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ashok
+ */
+public class OdfRefactorTest {
+    OdfRefactor ref = null;
+
+    public OdfRefactorTest() {
+              
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+        ref = new OdfRefactor("/Users/ashok/NetbeansProjects/SectionRefactorPlugin/testdocs/ken_bill_2009_1_10_eng_main.odt");
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+   
+
+    /**
+     * Test of saveDocument method, of class OdfRefactor.
+     */
+    @Test
+    public void testSaveDocument() {
+        try {
+            boolean b = ref.moveSectionBefore("part1", "preamble");
+            boolean result = ref.saveDocument();
+            assertEquals(result, true);
+            // TODO review the generated test code and remove the default call to fail.
+        } catch (Exception ex) {
+            Logger.getLogger(OdfRefactorTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+
+}
