0d2abd664a95864b00ab4668a86439ca79d3c166
ashok.hariharan <kohsah@users.noreply.github.com>
2009-02-20 08:37:49 +0000

    fixed bug in installer for windows

--- BungeniEditor/trunk/BungeniEditorClient/installer/runEditor.bat
+++ BungeniEditor/trunk/BungeniEditorClient/installer/runEditor.bat
@@ -1,4 +1,2 @@
-set JAVA_HOME=$JAVA_HOME
-REM set LOG4J_PROPS="file:///e:/projects/workingprojects/bungenieditor/trunk/BungeniEditorClient/dist/settings/log4j.properties"
-set LOG4J_PROPS="file:///$INSTALL_PATH/base/settings/log4j.properties"
+set JAVA_HOME=$JAVA_HOMEset LOG4J_PROPS=log4jdev.properties
 %JAVA_HOME%\bin\java -Dlog4j.ignoreTCL -Dlog4j.configuration=%LOG4J_PROPS% -jar .\base\BungeniEditorClient.jar
--- BungeniEditor/trunk/BungeniEditorClient/installer/runEditor.sh
+++ BungeniEditor/trunk/BungeniEditorClient/installer/runEditor.sh
@@ -1,4 +1,4 @@
 export JAVA_HOME=$JAVA_HOME
 export BUNGENI_EDITOR_HOME=$INSTALL_PATH/base
-export LOG4J_PROPS=file://$BUNGENI_EDITOR_HOME/settings/log4j.properties
+export LOG4J_PROPS=log4jdev.properties
 $JAVA_HOME/bin/java -Dlog4j.ignoreTCL=true -Dlog4j.configuration=$LOG4J_PROPS -jar ./base/BungeniEditorClient.jar
