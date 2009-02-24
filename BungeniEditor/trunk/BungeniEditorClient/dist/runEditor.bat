af6245cd9fdfc590ff009474618057f87b7d305f
ashok.hariharan <kohsah@users.noreply.github.com>
2009-02-24 13:40:16 +0000

    updated batch file to launch editor on windows

--- BungeniEditor/trunk/BungeniEditorClient/dist/runEditor.bat
+++ BungeniEditor/trunk/BungeniEditorClient/dist/runEditor.bat
@@ -1,4 +1,4 @@
 set JAVA_HOME=e:\Java\jdk1.6.0
-set LOG4J_PROPS="file:///e:/projects/workingprojects/bungenieditor/trunk/BungeniEditorClient/dist/settings/log4j.properties"
+set LOG4J_PROPS=log4jdev.properties
 set EDITOR_MEM=128m
 %JAVA_HOME%\bin\java -Dlog4j.ignoreTCL -Dlog4j.configuration=%LOG4J_PROPS% -Xmx%EDITOR_MEM% -jar BungeniEditorClient.jar
