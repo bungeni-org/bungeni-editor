8d5a3fbeb3f3e1e32ab69d514df98959591cf0a6
ashok.hariharan <kohsah@users.noreply.github.com>
2009-03-02 14:08:35 +0000

    abstract base class which a plugin can extend

--- plugins/BungeniEditorExternalPlugin/src/org/bungeni/editor/plugin/EditorPlugin.java
+++ plugins/BungeniEditorExternalPlugin/src/org/bungeni/editor/plugin/EditorPlugin.java
@@ -7,10 +7,24 @@
 
 package org.bungeni.editor.plugin;
 
+import javax.swing.JFrame;
+import org.bungeni.editor.plugin.impl.IEditorPluginAll;
+import org.bungeni.ooo.OOComponentHelper;
+
 /**
  *
  * @author undesa
  */
-public class EditorPlugin {
+public abstract class EditorPlugin implements IEditorPluginAll {
+
+    public abstract void setInstallDirectory(String path);
+
+    public abstract String getInstallDirectory() ;
+
+    public abstract boolean invoke() ;
+
+    public abstract void setOOComponentHelper(OOComponentHelper ooDocument) ;
+
+    public abstract void setParentFrame(JFrame frame);
 
 }
