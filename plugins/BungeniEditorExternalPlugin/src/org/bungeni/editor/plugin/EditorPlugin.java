a91a4762c31dea3195f43583196dd4ada24e2e80
ashok.hariharan <kohsah@users.noreply.github.com>
2009-03-03 09:13:34 +0000

    added methods and properties to editorplugin base class

--- plugins/BungeniEditorExternalPlugin/src/org/bungeni/editor/plugin/EditorPlugin.java
+++ plugins/BungeniEditorExternalPlugin/src/org/bungeni/editor/plugin/EditorPlugin.java
@@ -7,6 +7,7 @@
 
 package org.bungeni.editor.plugin;
 
+import java.util.HashMap;
 import javax.swing.JFrame;
 import org.bungeni.editor.plugin.impl.IEditorPluginAll;
 import org.bungeni.ooo.OOComponentHelper;
@@ -16,15 +17,39 @@ import org.bungeni.ooo.OOComponentHelper;
  * @author undesa
  */
 public abstract class EditorPlugin implements IEditorPluginAll {
+    
+    protected String installDirectory;
+    protected OOComponentHelper ooDocument ;
+    protected JFrame frame;
+    protected HashMap<String,Object> paramaterMap = new HashMap<String, Object>();
+    
+    public  void setInstallDirectory(String path){
+        this.installDirectory = path;
+    }
 
-    public abstract void setInstallDirectory(String path);
-
-    public abstract String getInstallDirectory() ;
+    public  String getInstallDirectory() {
+        return installDirectory;
+    }
 
     public abstract boolean invoke() ;
 
-    public abstract void setOOComponentHelper(OOComponentHelper ooDocument) ;
+    public void setOOComponentHelper(OOComponentHelper ooDocument) {
+        this.ooDocument = ooDocument;
+    }
 
-    public abstract void setParentFrame(JFrame frame);
+    public void setParentFrame(JFrame frame){
+        this.frame = frame;
+    }
+    
+    public void addParameter(String paramName, Object parameterValue) {
+        paramaterMap.put(paramName, parameterValue);
+    }
 
+    public  Object getParameter(String paramName) {
+        return this.paramaterMap.get(paramName);
+    }
+    
+    public final HashMap<String,Object> getParameters() {
+        return this.paramaterMap;
+    }
 }
