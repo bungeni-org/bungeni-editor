91f61374abfa2890cda289af78f09eb75cc5c812
ashok.hariharan <kohsah@users.noreply.github.com>
2009-03-04 16:02:03 +0000

    added instance factory - to instantiate class based on interface

--- plugins/BungeniEditorExternalPlugin/src/org/bungeni/editor/plugin/EditorPlugin.java
+++ plugins/BungeniEditorExternalPlugin/src/org/bungeni/editor/plugin/EditorPlugin.java
@@ -9,6 +9,7 @@ package org.bungeni.editor.plugin;
 
 import java.util.HashMap;
 import javax.swing.JFrame;
+import org.apache.log4j.Logger;
 import org.bungeni.editor.plugin.impl.IEditorPluginAll;
 import org.bungeni.ooo.OOComponentHelper;
 
@@ -22,7 +23,8 @@ public abstract class EditorPlugin implements IEditorPluginAll {
     protected OOComponentHelper ooDocument ;
     protected JFrame frame;
     protected HashMap<String,Object> paramaterMap = new HashMap<String, Object>();
-    
+    private static org.apache.log4j.Logger log = Logger.getLogger(EditorPlugin.class.getName());
+  
     public  void setInstallDirectory(String path){
         this.installDirectory = path;
     }
@@ -52,4 +54,23 @@ public abstract class EditorPlugin implements IEditorPluginAll {
     public final HashMap<String,Object> getParameters() {
         return this.paramaterMap;
     }
+    
+    public static IEditorPluginAll makeInstance(String classPath) {
+             IEditorPluginAll pPlugin = null;
+            try {
+                Class containerPanel = Class.forName(classPath);
+                pPlugin = (IEditorPluginAll) containerPanel.newInstance();
+             } catch (InstantiationException ex) {
+               log.debug("makeInstance :"+ ex.getMessage());
+               } catch (IllegalAccessException ex) {
+               log.debug("makeInstance :"+ ex.getMessage());
+               }  catch (ClassNotFoundException ex) {
+               log.debug("makeInstance :"+ ex.getMessage());
+              } catch (NullPointerException ex) {
+               log.debug("makeInstance :"+ ex.getMessage());
+              } finally {
+                  return pPlugin;
+              }
+        }
+
 }
