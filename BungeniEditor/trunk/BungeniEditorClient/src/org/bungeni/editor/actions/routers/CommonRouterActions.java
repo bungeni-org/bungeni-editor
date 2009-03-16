911b1a77cbf4de1519b17d446524d31c972984a4
ashok.hariharan <kohsah@users.noreply.github.com>
2009-03-16 15:04:07 +0000

    Moved common section creation APIs to this class / Added a parameter to set JFrame style on launch for selector dialogs.

--- BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/editor/actions/routers/CommonRouterActions.java
+++ BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/editor/actions/routers/CommonRouterActions.java
@@ -9,18 +9,32 @@
 
 package org.bungeni.editor.actions.routers;
 
+import com.sun.star.beans.XPropertySet;
+import com.sun.star.container.XNamed;
+import com.sun.star.text.XText;
+import com.sun.star.text.XTextContent;
+import com.sun.star.text.XTextCursor;
+import com.sun.star.text.XTextRange;
+import com.sun.star.text.XTextSection;
+import com.sun.star.text.XTextViewCursor;
+import java.util.HashMap;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
 import org.bungeni.editor.actions.toolbarAction;
 import org.bungeni.editor.actions.toolbarSubAction;
+import org.bungeni.editor.document.DocumentSection;
+import org.bungeni.editor.document.DocumentSectionsContainer;
 import org.bungeni.editor.selectors.BaseMetadataContainerPanel;
 import org.bungeni.editor.selectors.DialogSelectorFactory;
 import org.bungeni.editor.selectors.IDialogSelector;
 import org.bungeni.editor.selectors.IMetadataContainerPanel;
+import org.bungeni.editor.selectors.metadata.SectionMetadataEditor;
 import org.bungeni.error.BungeniMsg;
 import org.bungeni.error.BungeniValidatorState;
 import org.bungeni.ooo.OOComponentHelper;
+import org.bungeni.ooo.ooQueryInterface;
+import org.bungeni.ooo.utils.CommonExceptionUtils;
 
 /**
  *
@@ -88,8 +102,12 @@ public class CommonRouterActions {
     }
    
     static class displaySubActionFrameRunner extends SwingRunner {
-        public displaySubActionFrameRunner(toolbarAction a, toolbarSubAction sa, JFrame pf, OOComponentHelper ooDoc){
+        
+        boolean b_alwaysOnTop = false;
+        
+        public displaySubActionFrameRunner(toolbarAction a, toolbarSubAction sa, JFrame pf, OOComponentHelper ooDoc, boolean alwaysOnTop){
             super (a, sa, pf, ooDoc);
+            b_alwaysOnTop = alwaysOnTop;
         }
         @Override
         public void run() {
@@ -110,8 +128,8 @@ public class CommonRouterActions {
                     f.add(containerPanel.getPanelComponent());
                     f.pack();
                     f.setLocationRelativeTo(null);
+                    f.setAlwaysOnTop(b_alwaysOnTop);   
                     f.setVisible(true);
-                    //f.setAlwaysOnTop(true);   
                    // f.setVisible(true);
                 } catch (Exception ex){
                     log.error("displaySubActionFrameRunner exception :" + ex.getMessage());
@@ -152,11 +170,13 @@ public class CommonRouterActions {
         
     }
     
-    public static BungeniValidatorState displaySubActionDialog(toolbarAction action, toolbarSubAction subAction, JFrame parentFrame, OOComponentHelper ooDocument) {
+    
+    
+    public static BungeniValidatorState displaySubActionDialog(toolbarAction action, toolbarSubAction subAction, JFrame parentFrame, OOComponentHelper ooDocument, boolean alwaysOnTop) {
                BungeniValidatorState returnState = null;
                 try {
 
-                    displaySubActionFrameRunner dsfRunner = new displaySubActionFrameRunner( action,  subAction, parentFrame, ooDocument);
+                    displaySubActionFrameRunner dsfRunner = new displaySubActionFrameRunner( action,  subAction, parentFrame, ooDocument, alwaysOnTop);
                     javax.swing.SwingUtilities.invokeLater(dsfRunner);
                     returnState = new BungeniValidatorState(true, new BungeniMsg("SUCCESS")); 
 
@@ -183,7 +203,6 @@ public class CommonRouterActions {
                 } finally {
                     return returnState;
                 }
-        
     }
     
     
@@ -203,7 +222,7 @@ public class CommonRouterActions {
                 }
         }
     
-        public static BungeniValidatorState displayFilteredDialog(toolbarAction action, toolbarSubAction subAction, OOComponentHelper ooDocument) {
+   public static BungeniValidatorState displayFilteredDialog(toolbarAction action, toolbarSubAction subAction, OOComponentHelper ooDocument) {
             BungeniValidatorState returnState =  null;
            try {
              log.debug("displayFilteredDialog: subAction name = "+ subAction.sub_action_name());
@@ -234,4 +253,109 @@ public class CommonRouterActions {
                    return returnState; 
              }
     }
+
+    public static String get_newSectionNameForAction(toolbarAction pAction, OOComponentHelper ooDocument) {
+         String newSectionName = "";
+         if (pAction.action_numbering_convention().equals("single")) {
+             newSectionName = pAction.action_naming_convention();
+         } else if (pAction.action_numbering_convention().equals("serial")) {
+             String sectionPrefix = pAction.action_naming_convention();
+             for (int i=1 ; ; i++) {
+                if (ooDocument.hasSection(sectionPrefix+i)) {
+                    continue;
+                } else {
+                    newSectionName = sectionPrefix+i;
+                    break;
+                }
+             }
+           
+         } else {
+             log.error("get_newSectionNameForAction: invalid action naming convention: "+ pAction.action_naming_convention());
+         }
+         return newSectionName;
+    }
+
+ public static boolean action_createSystemContainerFromSelection(OOComponentHelper ooDoc, String systemContainerName){
+        boolean bResult = false; 
+        try {
+        XTextViewCursor xCursor = ooDoc.getViewCursor();
+        XText xText = xCursor.getText();
+        XTextContent xSectionContent = ooDoc.createTextSection(systemContainerName, (short)1);
+        xText.insertTextContent(xCursor, xSectionContent , true); 
+        bResult = true;
+        } catch (com.sun.star.lang.IllegalArgumentException ex) {
+            bResult = false;
+            log.error("in addTextSection : "+ex.getLocalizedMessage(), ex);
+        }  finally {
+            return bResult;
+        }
+    }
+
+     public static boolean action_createSpannedContainer(OOComponentHelper ooDocument, String sectionName, String startBookmark, String endBookmark) {
+        boolean bState = false;
+        try {
+            
+            //get source and target bookmark
+            Object sBookmark = ooDocument.getBookmarks().getByName(startBookmark);
+            Object eBookmark = ooDocument.getBookmarks().getByName(endBookmark);
+            
+            //get bookmark ranges
+            XTextContent xstartContent = ooQueryInterface.XTextContent(sBookmark);
+            XTextContent xendContent = ooQueryInterface.XTextContent(eBookmark);
+            XTextRange xstartRange = xstartContent.getAnchor();
+            XTextRange xendRange =xendContent.getAnchor();
+            
+            //get cursor spanning bookmark ranges
+            XTextCursor scaleCursor = ooDocument.getTextDocument().getText().createTextCursor();
+            scaleCursor.gotoRange(xstartRange, false);
+            scaleCursor.gotoRange(xendRange, true);
+            
+            //create section over spanned range
+            XText xDocText = ooDocument.getTextDocument().getText();
+            XTextContent sectionContent = ooDocument.createTextSection(sectionName, (short)1);
+            xDocText.insertTextContent(scaleCursor, sectionContent, true);
+            //revert to true
+            bState = true;
+        } catch(Exception ex) {
+            log.error("action_createSpannedContainer :" + ex.getMessage());
+        } finally {
+            return bState;
+        }
+     }
+     
+     
+     public static HashMap<String,String> get_newSectionMetadata(toolbarAction pAction) {
+         HashMap<String,String> metaMap = new HashMap<String,String>();
+         metaMap.put("BungeniSectionType", pAction.action_section_type());
+         metaMap.put(SectionMetadataEditor.MetaEditableFlag, "false");
+         return metaMap;
+     }
+
+    public static void setSectionProperties(toolbarAction pAction, String newSectionName, OOComponentHelper ooDocument) {
+        String sectionType = pAction.action_section_type();
+        DocumentSection secObj = DocumentSectionsContainer.getDocumentSectionByType(sectionType);
+        HashMap<String,Object> sectionProps = secObj.getSectionProperties();
+        XTextSection newSection = ooDocument.getSection(newSectionName);
+        XNamed namedSection = ooQueryInterface.XNamed(newSection);
+        XPropertySet xProps = ooQueryInterface.XPropertySet(newSection);
+        for (String propName: sectionProps.keySet()) {
+             try {
+                log.debug("setSectionProperties : "+ propName + " value = " + sectionProps.get(propName).toString());
+                Object propVal = sectionProps.get(propName);
+                if (propVal.getClass() == java.lang.Integer.class) {
+                      xProps.setPropertyValue(propName, (java.lang.Integer) sectionProps.get(propName));
+                } else if (propVal.getClass() == java.lang.Long.class) {
+                      xProps.setPropertyValue(propName, (java.lang.Long) sectionProps.get(propName));               
+                } else if (propVal.getClass() == java.lang.String.class) {
+                      xProps.setPropertyValue(propName, (java.lang.String) sectionProps.get(propName));
+                } else
+                      xProps.setPropertyValue(propName, (java.lang.String) sectionProps.get(propName));
+            } catch (Exception ex) {
+                log.error("setSectionProperties :"+ propName +" : "  +ex.getMessage());
+                log.error("setSectionProperties :"+ CommonExceptionUtils.getStackTrace(ex));
+            } 
+        }
+    }
+
+
 }
