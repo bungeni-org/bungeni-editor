aa4f6d735ebf8db0096aca76432ba90aa3fa9a1d
ashok.hariharan <kohsah@users.noreply.github.com>
2009-03-19 13:17:50 +0000

    Updated IzPack installer with windows fixes (encoding for runEditor.bat / quoting path names)

--- BungeniEditor/trunk/BungeniEditorClient/installer/runEditor.bat
+++ BungeniEditor/trunk/BungeniEditorClient/installer/runEditor.bat
@@ -1,2 +1,2 @@
-set JAVA_HOME=$JAVA_HOMEset LOG4J_PROPS=log4jdev.properties
+set JAVA_HOME="$JAVA_HOME"set LOG4J_PROPS=log4jdev.properties
 %JAVA_HOME%\bin\java -Dlog4j.ignoreTCL -Dlog4j.configuration=%LOG4J_PROPS% -jar .\base\BungeniEditorClient.jar
---
+++ BungeniEditor/trunk/BungeniEditorClient/installer/settingsEditor.sh
@@ -0,0 +1,2 @@
+export JAVA_HOME=$JAVA_HOME
+$JAVA_HOME/bin/java -cp ./base/lib/h2.jar org.h2.tools.Server
