e1039ce0798a696c20a5ee74c07977dd7be7cb48
ashok.hariharan <kohsah@users.noreply.github.com>
2009-03-25 15:03:32 +0000

    UI to display structural error messages

---
+++ plugins/BungeniStructuralRules/src/org/bungeni/editor/rules/ui/StructuralErrorUI.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.bungeni.editor.rules.ui;
+
+/**
+ *
+ * @author undesa
+ */
+public class StructuralErrorUI {
+
+}
