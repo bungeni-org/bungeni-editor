92e2b18b40265592818dc417b628107558323900
ashok.hariharan <kohsah@users.noreply.github.com>
2009-03-26 08:15:36 +0000

    Added launch() api to abstract launching of JFrames

--- BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/utils/BungeniFrame.java
+++ BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/utils/BungeniFrame.java
@@ -5,15 +5,22 @@
 
 package org.bungeni.utils;
 
+import java.awt.Dimension;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 /**
  *
  * @author undesa
  */
 public class BungeniFrame extends JFrame {
-       
+
+        public static int BUNGENIFRAME_CLOSE_STYLE = JFrame.DISPOSE_ON_CLOSE;
+        public static boolean BUNGENIFRAME_RESIZABLE = true;
+        public static boolean BUNGENIFRAME_ALWAYS_ON_TOP = false;
+
+
         public BungeniFrame(){
             super();
             setIconForFrame();
@@ -23,10 +30,31 @@ public class BungeniFrame extends JFrame {
             super(titleStr);
             setIconForFrame();
         }
+
+        public BungeniFrame(String titleStr, Dimension frmSize) {
+            super(titleStr);
+            setIconForFrame();
+            this.setSize(frmSize);
+        }
         
         private void setIconForFrame(){
            ImageIcon iconApp = CommonTreeFunctions.loadIcon("bungeni.jpg");
            setIconImage(iconApp.getImage());
+
+        }
+
+        /**
+         * API to launch a frame with a panel and of a particular size
+         * @param panel - JPanel to be added to the frame
+         * @param dSize - size of the JFrame
+         */
+        public void launch(JPanel panel, Dimension dSize){
+           this.setDefaultCloseOperation(BUNGENIFRAME_CLOSE_STYLE);
+           this.add(panel);
+           this.setSize(dSize);
+           this.setResizable(BUNGENIFRAME_RESIZABLE);
+           this.setAlwaysOnTop(BUNGENIFRAME_ALWAYS_ON_TOP);
+           this.setVisible(true);
         }
 
 }
