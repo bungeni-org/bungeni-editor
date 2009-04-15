e472b51a025aabdf1f6e3ad7dea826337a4195ed
ashok.hariharan <kohsah@users.noreply.github.com>
2009-04-15 11:54:52 +0000

    Updated scripts for installer

--- BungeniEditor/trunk/BungeniEditorClient/installer/runEditor.bat
+++ BungeniEditor/trunk/BungeniEditorClient/installer/runEditor.bat
@@ -1,2 +1,4 @@
-set JAVA_HOME="$JAVA_HOME"set LOG4J_PROPS=log4jdev.properties
-%JAVA_HOME%\bin\java -Dlog4j.ignoreTCL -Dlog4j.configuration=%LOG4J_PROPS% -jar .\base\BungeniEditorClient.jar
+set JAVA_HOME="$JAVA_HOME"
+set LOG4J_PROPS=log4jdev.properties
+set EDITOR_MEM=196m
+%JAVA_HOME%\bin\java -Dlog4j.ignoreTCL -Dlog4j.configuration=%LOG4J_PROPS%  -Xmx%EDITOR_MEM% -jar .\base\BungeniEditorClient.jar
--- BungeniEditor/trunk/BungeniEditorClient/installer/runEditor.sh
+++ BungeniEditor/trunk/BungeniEditorClient/installer/runEditor.sh
@@ -1,4 +1,5 @@
 export JAVA_HOME=$JAVA_HOME
 export BUNGENI_EDITOR_HOME=$INSTALL_PATH/base
 export LOG4J_PROPS=log4jdev.properties
-$JAVA_HOME/bin/java -Dlog4j.ignoreTCL=true -Dlog4j.configuration=$LOG4J_PROPS -jar ./base/BungeniEditorClient.jar
+export EDITOR_MEM=196m
+$JAVA_HOME/bin/java -Dlog4j.ignoreTCL=true -Dlog4j.configuration=$LOG4J_PROPS  -Xmx$EDITOR_MEM  -jar ./base/BungeniEditorClient.jar
