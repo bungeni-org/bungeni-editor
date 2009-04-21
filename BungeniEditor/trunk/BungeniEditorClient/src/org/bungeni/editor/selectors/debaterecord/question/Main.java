efb4bad8b385f06cb3613a5bd27436a39e73d709
ashok.hariharan <kohsah@users.noreply.github.com>
2009-04-21 21:33:53 +0000

    Resolved update conflict

--- BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/editor/selectors/debaterecord/question/Main.java
+++ BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/editor/selectors/debaterecord/question/Main.java
@@ -1,12 +1,5 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package org.bungeni.editor.selectors.debaterecord.question;
 
-import com.sun.star.beans.XPropertySet;
-import com.sun.star.container.XNamed;
 import com.sun.star.text.XText;
 import com.sun.star.text.XTextContent;
 import com.sun.star.text.XTextSection;
@@ -16,11 +9,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import javax.swing.JFrame;
 import org.bungeni.editor.actions.routers.CommonRouterActions;
-import org.bungeni.editor.document.DocumentSection;
-import org.bungeni.editor.document.DocumentSectionsContainer;
 import org.bungeni.editor.selectors.BaseMetadataContainerPanel;
-import org.bungeni.ooo.ooQueryInterface;
-import org.bungeni.ooo.utils.CommonExceptionUtils;
 
 /**
  *
@@ -33,7 +22,7 @@ public class Main extends BaseMetadataContainerPanel {
     public Main(){
         super();
     }
-    
+
     @Override
     protected void setupPanels() {
        m_allPanels = new ArrayList<panelInfo>(){
@@ -45,7 +34,7 @@ public class Main extends BaseMetadataContainerPanel {
                     add(new panelInfo("QuestionText", "org.bungeni.editor.selectors.debaterecord.question.QuestionText"));
                 }
         };
-    
+
        m_activePanels = new ArrayList<panelInfo>(){
             {
                     add(new panelInfo("QuestionSelect", "org.bungeni.editor.selectors.debaterecord.question.QuestionSelect"));
@@ -70,7 +59,7 @@ public class Main extends BaseMetadataContainerPanel {
     public Component getPanelComponent() {
         return this;
     }
-    
+
     @Override
     public void updateAllPanels(){
         try {
@@ -81,16 +70,16 @@ public class Main extends BaseMetadataContainerPanel {
             log.error("updateAllPanels : " + ex.getMessage());
         }
     }
-    
+
     private final short SECTION_COLUMNS = 1;
     private String m_questionSectionName = "";
-    
+
     @Override
     public boolean preApplySelectedInsert(){
         //
         makeMetaEditable();
         //create the section if it doesnt exist over here...
-        
+
         String newSection = getActionSectionName();
         if (!ooDocument.hasSection(newSection)) {
             //creat ethe new section
@@ -99,7 +88,7 @@ public class Main extends BaseMetadataContainerPanel {
             XText xText = xCursor.getText();
             XTextContent xSectionContent = ooDocument.createTextSection(newSection, SECTION_COLUMNS);
             try {
-            xText.insertTextContent(xCursor, xSectionContent , true); 
+            xText.insertTextContent(xCursor, xSectionContent , true);
             } catch (com.sun.star.lang.IllegalArgumentException ex) {
                 log.error("IllegalArgumentException, preMainApply : " + ex.getMessage());
             }
@@ -107,44 +96,12 @@ public class Main extends BaseMetadataContainerPanel {
             //get section poperties
             XTextSection xSection = ooDocument.getSection(newSection);
             CommonRouterActions.setSectionProperties(theAction, newSection, ooDocument);
-           /*** to be deleted April 16
-            XNamed namedSection = ooQueryInterface.XNamed(xSection);
-            XPropertySet xProps = ooQueryInterface.XPropertySet(xSection);
-            //container section was created here ...
-             String sectionType = theAction.action_section_type();
-             DocumentSection secObj = DocumentSectionsContainer.getDocumentSectionByType(sectionType);
-             HashMap<String,Object> sectionProps = secObj.getSectionProperties(ooDocument);
-             //update properties...
-             for (String propName: sectionProps.keySet()) {
-             try {
-                    //log.debug("setSectionProperties : "+ propName + " value = " + sectionProps.get(propName).toString());
-                    //xProps.setPropertyValue(propName, sectionProps.get(propName));
-                    Object propVal = sectionProps.get(propName);
-                    if (propVal.getClass() == java.lang.Integer.class) {
-                          xProps.setPropertyValue(propName, (java.lang.Integer) sectionProps.get(propName));
-                    } else if (propVal.getClass() == java.lang.Long.class) {
-                          xProps.setPropertyValue(propName, (java.lang.Long) sectionProps.get(propName));               
-                    } else if (propVal.getClass() == java.lang.String.class) {
-                          xProps.setPropertyValue(propName, (java.lang.String) sectionProps.get(propName));
-                    } else
-                          xProps.setPropertyValue(propName, (java.lang.String) sectionProps.get(propName));
-                } catch (Exception ex) {
-                log.error("setSectionProperties :"+ propName +" : "  +ex.getMessage());
-                log.error("setSectionProperties :"+ CommonExceptionUtils.getStackTrace(ex));
-                } 
-            }
-            *****/
         ooDocument.setSectionMetadataAttributes(xSection, CommonRouterActions.get_newSectionMetadata(theAction));
 
-            /**
-            HashMap<String,String> metaMap = new HashMap<String,String>();
-            metaMap.put("BungeniSectionType", theAction.action_section_type());
-            ooDocument.setSectionMetadataAttributes(xSection, metaMap);
-            **/
            }
         return true;
     }
-    
+
     @Override
     public boolean postApplySelectedInsert(){
                 if (sectionMetadataEditor != null ) {
@@ -159,7 +116,7 @@ public class Main extends BaseMetadataContainerPanel {
         }
         return true;
     }
-    
+
     @SuppressWarnings("empty-statement")
         public String getActionSectionName() {
         //get the action naming convention
@@ -170,7 +127,7 @@ public class Main extends BaseMetadataContainerPanel {
             //get highest section name possible
             int iStart = 1;
             for (; ooDocument.hasSection(theAction.action_naming_convention()+iStart); iStart++ );
-            return theAction.action_naming_convention()+iStart; 
+            return theAction.action_naming_convention()+iStart;
         } else
             return theAction.action_naming_convention();
     }
