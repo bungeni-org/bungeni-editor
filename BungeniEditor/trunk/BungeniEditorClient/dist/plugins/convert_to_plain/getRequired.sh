31106b6c211f1613bd2223a94673b62b8f9a00fa
ashok.hariharan <kohsah@users.noreply.github.com>
2009-05-12 11:58:06 +0000

    Plugin to convert document to plain

---
+++ BungeniEditor/trunk/BungeniEditorClient/dist/plugins/convert_to_plain/getRequired.sh
@@ -0,0 +1,5 @@
+cp /home/undesa/Projects/Bungeni/BungeniOdfDom/dist/bungeniodfdom.jar .
+cp /home/undesa/Projects/Bungeni/BungeniSectionPlain/dist/BungeniSectionPlain.jar .
+cp /home/undesa/Projects/Bungeni/external_libs/xerces/xerces2.9.1/xercesImpl.jar .
+cp /home/undesa/Projects/Bungeni/external_libs/apache-log4j/log4j.jar .
+cp /home/undesa/Projects/Bungeni/external_libs/odfdom/odfdom.jar .
