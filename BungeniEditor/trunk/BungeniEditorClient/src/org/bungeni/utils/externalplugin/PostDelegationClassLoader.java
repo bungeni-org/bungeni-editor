8a487329b967f07fb48afd2a13e9ccf2e7b63d30
ashok.hariharan <kohsah@users.noreply.github.com>
2009-05-13 11:00:33 +0000

    post-delegation class loader used for loading plugin libraries. this loader is used to isolate classpath environments for external plugins - as version clashes can contaminate the jvm classloader and result in java linkage errrors.

---
+++ BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/utils/externalplugin/PostDelegationClassLoader.java
@@ -0,0 +1,53 @@
+package org.bungeni.utils.externalplugin;
+
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.net.URLStreamHandlerFactory;
+
+
+public class PostDelegationClassLoader extends URLClassLoader {
+
+    public PostDelegationClassLoader(URL[] urls, ClassLoader parent, URLStreamHandlerFactory factory) {
+        super(urls, parent, factory);
+    }
+
+    public PostDelegationClassLoader(URL[] urls, ClassLoader parent) {
+        super(urls, parent);
+    }
+
+    public PostDelegationClassLoader(URL[] urls) {
+        super(urls);
+    }
+
+    @Override
+    public Class<?> loadClass(String name) throws ClassNotFoundException {
+        // First check whether it's already been loaded
+        Class loadedClass = findLoadedClass(name);
+        // Nope, try to load it
+        if (loadedClass == null) {
+            try {
+                // Ignore parent delegation and just try to load locally
+                loadedClass = findClass(name);
+            } catch (ClassNotFoundException e) {
+                // Swallow - does not exist locally
+                System.out.println("loadClass " + e.getMessage());
+            }
+            // If not found, just use the standard URLClassLoader (which follows normal parent delegation)
+            if (loadedClass == null) {
+                // throws ClassNotFoundException if not found in delegation hierarchy at all
+                loadedClass = super.loadClass(name);
+            }
+        }
+        return loadedClass;
+    }
+
+    @Override
+        public URL getResource(String name) {
+        URL url = findResource(name);
+        if (null == url) {
+            url = super.findResource(name);
+        }
+
+        return url;
+    }
+}
