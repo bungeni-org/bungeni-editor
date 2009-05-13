06ee304fa0cf9724d279cf715969121a2fc6e44f
ashok.hariharan <kohsah@users.noreply.github.com>
2009-05-13 15:29:30 +0000

    Default sax driver property

--- BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/extutils/BungeniEditorProperties.java
+++ BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/extutils/BungeniEditorProperties.java
@@ -16,7 +16,8 @@ import org.bungeni.db.SettingsQueryFactory;
 public class BungeniEditorProperties {
     private static HashMap<String,String> propertiesMap = new HashMap<String,String>();
     private static org.apache.log4j.Logger log = Logger.getLogger(BungeniEditorProperties.class.getName());
-    
+
+    public static String SAX_PARSER_DRIVER = "org.apache.xerces.parsers.SAXParser";
     /** Creates a new instance of BungeniEditorProperties */
     public BungeniEditorProperties() {
     }
