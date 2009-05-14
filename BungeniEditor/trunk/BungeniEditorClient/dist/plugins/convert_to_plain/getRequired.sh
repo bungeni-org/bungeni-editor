b082ffbe5834486d677c6ef74d6e22bda7f24628
ashok.hariharan <kohsah@users.noreply.github.com>
2009-05-14 15:37:09 +0000

    Updated getRequired.sh

--- BungeniEditor/trunk/BungeniEditorClient/dist/plugins/convert_to_plain/getRequired.sh
+++ BungeniEditor/trunk/BungeniEditorClient/dist/plugins/convert_to_plain/getRequired.sh
@@ -3,3 +3,4 @@ cp /home/undesa/Projects/Bungeni/BungeniSectionPlain/dist/BungeniSectionPlain.ja
 cp /home/undesa/Projects/Bungeni/external_libs/xerces/xerces2.9.1/xercesImpl.jar .
 cp /home/undesa/Projects/Bungeni/external_libs/apache-log4j/log4j.jar .
 cp /home/undesa/Projects/Bungeni/external_libs/odfdom/odfdom.jar .
+cp /home/undesa/Projects/Bungeni/EditorPluginInterface/dist/editorplugininterface.jar .
