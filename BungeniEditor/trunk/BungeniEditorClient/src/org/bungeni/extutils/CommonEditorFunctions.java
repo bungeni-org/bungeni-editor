96545a7033c1a7a92845a06aac49ee61a08d9417
ashok.hariharan <kohsah@users.noreply.github.com>
2009-05-21 14:55:08 +0000

    helper API to convert relative to absolute paths

--- BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/extutils/CommonEditorFunctions.java
+++ BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/extutils/CommonEditorFunctions.java
@@ -6,14 +6,25 @@
 package org.bungeni.extutils;
 
 import java.io.File;
+import org.apache.log4j.Logger;
 import org.bungeni.db.DefaultInstanceFactory;
 
 /**
  *
- * @author undesa
+ * @author Ashok Hariharan
  */
 public class CommonEditorFunctions {
+
+   private static org.apache.log4j.Logger log = Logger.getLogger(CommonEditorFunctions.class.getName());
+
     public static String getSettingsFolder(){
         return DefaultInstanceFactory.DEFAULT_INSTALLATION_PATH() + File.separator + "settings";
+    }
+
+    public static String getPathRelativeToRoot(String thisPath) {
+        String runtimeRoot = System.getProperty("user.dir");
+        String appendPath = thisPath.replace('/', File.separatorChar);
+        log.info("getPathRelativetoRoot for (" + thisPath + ") is " + runtimeRoot + File.separator + appendPath );
+        return runtimeRoot + File.separator + appendPath ;
     }
 }
