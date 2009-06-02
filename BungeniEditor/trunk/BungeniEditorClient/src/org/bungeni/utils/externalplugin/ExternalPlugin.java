01701725860cf51b55836e98371995b7287e6562
ashok.hariharan <kohsah@users.noreply.github.com>
2009-06-02 07:46:29 +0000

    Added API to get access to the plugin classloader

--- BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/utils/externalplugin/ExternalPlugin.java
+++ BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/utils/externalplugin/ExternalPlugin.java
@@ -1,8 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package org.bungeni.utils.externalplugin;
 
 import java.lang.reflect.Constructor;
@@ -51,6 +46,10 @@ public class ExternalPlugin {
      */
     public void setPluginClassLoader(ClassLoader loader) {
         this.pluginClassLoader = loader;
+    }
+
+    public ClassLoader getPluginClassLoader() {
+        return this.pluginClassLoader;
     }
 
     /**
