430f1d9e00d8d8d840bc80d60dd0b5d9eb46f0c6
ashok.hariharan <kohsah@users.noreply.github.com>
2009-06-02 07:38:34 +0000

    Added commment for plugin config

--- plugins/BungeniOdfStructuralRules/src/org/bungeni/editor/rulesimpl/StructuralRulesConfig.java
+++ plugins/BungeniOdfStructuralRules/src/org/bungeni/editor/rulesimpl/StructuralRulesConfig.java
@@ -7,6 +7,7 @@ import java.io.File;
  * @author Ashok Hariharan
  */
 public class StructuralRulesConfig {
+        //usually settings/structural_rules
         public static String APPLN_PATH_PREFIX = "";
         public static String DOCSTRUCTURE_RULES_PATH_PREFIX   = "doc_rules"+ File.separator;
         public static String RULE_ENGINES_PATH_PREFIX = "engine_rules" + File.separator;
