b9c03215f4d80eaa89779fda451c7ae87011f197
ashok.hariharan <kohsah@users.noreply.github.com>
2009-06-04 09:41:23 +0000

    Added extended api to plugin

--- plugins/BungeniEditorPluginInterface/src/org/bungeni/plugins/IEditorPlugin.java
+++ plugins/BungeniEditorPluginInterface/src/org/bungeni/plugins/IEditorPlugin.java
@@ -7,6 +7,20 @@ import java.util.HashMap;
  * @author Ashok Hariharan
  */
 public interface IEditorPlugin {
+    /**
+     * Send input params to the plugin as a key-value hash-map
+     * @param params
+     */
     public void setParams(HashMap params);
+    /**
+     * A standard exec() api
+     * @return
+     */
     public String exec();
+    /**
+     * An extended api that allows passing in multiple parameters and returning an Object
+     * @param params
+     * @return
+     */
+    public Object exec2(Object[] params);
 }
