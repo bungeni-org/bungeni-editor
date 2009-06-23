2a9cfd139a1077d07225f456a14120232678b2f5
ashok.hariharan <kohsah@users.noreply.github.com>
2009-06-23 16:14:58 +0000

    Added support classes for invoking external applications

---
+++ BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/shell/ILogDevice.java
@@ -0,0 +1,10 @@
+
+package org.bungeni.shell;
+/**
+ *
+ * @author  M Venkataramana, http://www.javalobby.org/java/forums/t53333.html
+ */
+public interface ILogDevice
+{
+	public void log(String str);
+}
