0f5cefe8e9c6609d16ceb22943a1ccb5f1b915a9
ashok.hariharan <kohsah@users.noreply.github.com>
2009-06-24 16:32:24 +0000

    Replaced direct transformation calls with REST calls

--- BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/ooo/transforms/loadable/AnXmlTransform.java
+++ BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/ooo/transforms/loadable/AnXmlTransform.java
@@ -9,27 +9,43 @@
 
 package org.bungeni.ooo.transforms.loadable;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.bungeni.db.DefaultInstanceFactory;
 import org.bungeni.extutils.BungeniEditorProperties;
+import org.bungeni.extutils.BungeniEditorPropertiesHelper;
 import org.bungeni.ooo.OOComponentHelper;
 import org.bungeni.ooo.transforms.impl.BungeniDocTransform;
 import org.bungeni.ooo.utils.CommonExceptionUtils;
 import org.bungeni.extutils.MessageBox;
+import org.bungeni.restlet.client.TransformerClient;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.jdom.input.SAXBuilder;
+import org.jdom.xpath.XPath;
 /**
  *
  * @author Administrator
  */
 public class AnXmlTransform extends BungeniDocTransform {
         private static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(AnXmlTransform.class.getName());
-    /** Creates a new instance of HTMLTransform */
+        TransformerClient transformerClient ;
+        private static final String TRANSFORM_OUTPUT_FILE = "transform_output.xml";
+
+        /** Creates a new instance of HTMLTransform */
     public AnXmlTransform() {
         super();
+        transformerClient = new TransformerClient();
     }
 
     
@@ -82,21 +98,13 @@ public class AnXmlTransform extends BungeniDocTransform {
     String EXPORT_OUTPUT_FILE  = "";
     public boolean transform(OOComponentHelper ooDocument) {
         boolean bState = false;
-        //save the current class loader
-        final ClassLoader savedClassLoader = Thread.currentThread().getContextClassLoader();
         try {
-       //      XStorable docStore =ooDocument.getStorable();
-       //     String urlString = (String) getParams().get("StoreToUrl");
-       //     docStore.storeToURL(urlString, getTransformProps().toArray(new PropertyValue[getTransformProps().size()]));
-           
             //1 - query interface the document for the storable url
             
             File fopenDocumentFile  = null ;
             if (ooDocument.isDocumentOnDisk())  {
                 String sDocUrl = ooDocument.getDocumentURL();
                 //set the path to the output xml file
-                
-                //EXPORT_OUTPUT_FILE = DefaultInstanceFactory.DEFAULT_INSTALLATION_PATH()+ File.separator + "workspace" + File.separator + "export" + File.separator + "result.xml";
                 
                 fopenDocumentFile = convertUrlToFile(sDocUrl);
                 String fullFileName = fopenDocumentFile.getName();
@@ -107,23 +115,116 @@ public class AnXmlTransform extends BungeniDocTransform {
                 defaultSavePath = defaultSavePath.replace('/', File.separatorChar);
          
                 EXPORT_OUTPUT_FILE = fopenDocumentFile.getParentFile().getPath()+File.separator + pref+ ".xml";
-         
-        
+                //get temporary output file
+                File fOut = new File(EXPORT_OUTPUT_FILE);
+                File ftmpOutput = new File(fOut.getParent() + File.separator + TRANSFORM_OUTPUT_FILE );
+
                 String pathPrefix = DefaultInstanceFactory.DEFAULT_INSTALLATION_PATH() + File.separator + "transformer" + File.separator;
-                    //MessageBox.OK(this.callerFrame, "Document transformation failed");
+                //call the transformer server
+                callTransform(fopenDocumentFile.getPath(), ftmpOutput.getPath());
+                if (ftmpOutput.exists()) {
+                    processOutputFile(ftmpOutput, fOut);
+                    bState = true;
+                }
+
             } else {
                 MessageBox.OK("Please save the document before trying to transform it!");
                 bState = false;
             }
-           // bState= true;
         } catch (Exception ex) {
             log.error("transform : " + ex.getMessage());
             log.error("transform : " + CommonExceptionUtils.getStackTrace(ex));
+        }
+        return bState;
+    }
+
+
+    /**
+     * API that calls the REST server ... this is run in a context class loader 
+     * @param inputDocPath
+     * @param tmpoutputFile
+     */
+    private void callTransform(String inputDocPath, String tmpoutputFile) {
+        final ClassLoader savedClassLoader = Thread.currentThread().getContextClassLoader();
+        FileOutputStream fostream  = null;
+        try {
+             Thread.currentThread().setContextClassLoader(getClass().getClassLoader());
+             if (transformerClient.isServerRunning()) {
+                 transformerClient.postParams(BungeniEditorPropertiesHelper.getCurrentDocType(), "odt2akn");
+                  fostream = new FileOutputStream(new File(tmpoutputFile));
+                 transformerClient.postFile(inputDocPath, fostream);
+             }
+        } catch (Exception ex) {
+            log.error("callTransform ", ex);
         } finally {
-            Thread.currentThread().setContextClassLoader(savedClassLoader);
-            return bState;
+             Thread.currentThread().setContextClassLoader(savedClassLoader);
+             try {
+                  if (fostream != null)
+                     fostream.close();
+              } catch (IOException ex) {
+                      log.error("callTransform : ", ex);
+              }
         }
     }
-    
+
+    private SAXBuilder outputBuilder ;
+
+    private String TRANSFORM_ERRORS = "/TransformerResponse/transformResult/errors";
+
+    private String TRANSFORM_OUTPUT = "/TransformerResponse/transformResult/output";
+
+    private String getErrors(Document xmlDoc) {
+        String foundErrors = "";
+        try {
+            XPath errorPath = XPath.newInstance(TRANSFORM_ERRORS);
+            Element errorElement = (Element) errorPath.selectSingleNode(xmlDoc);
+            foundErrors =  errorElement.getValue();
+            foundErrors = foundErrors.trim();
+        } catch (JDOMException ex) {
+            log.error("getErrors :", ex);
+        }
+        return foundErrors;
+    }
+
+     private String getOutput(Document xmlDoc) {
+        String foundOut = "";
+        try {
+            XPath errorPath = XPath.newInstance(TRANSFORM_OUTPUT);
+            Element outputElem = (Element) errorPath.selectSingleNode(xmlDoc);
+            foundOut =  outputElem.getValue();
+            foundOut = foundOut.trim();
+        } catch (JDOMException ex) {
+            log.error("getOutput :", ex);
+        }
+        return foundOut;
+    }
+
+    private void processOutputFile(File tmpoutputFile, File fOutputFile) {
+        try {
+            if (outputBuilder == null ) 
+                        outputBuilder = new SAXBuilder(BungeniEditorProperties.SAX_PARSER_DRIVER, false);
+            Document transDoc = outputBuilder.build(tmpoutputFile);
+            //first get errors
+            String outputErrors = getErrors(transDoc);
+            //get the output file
+            String outputfile = getOutput(transDoc);
+            //write the output file
+            FileWriter fw = new FileWriter(fOutputFile);
+            BufferedWriter bOut = new BufferedWriter(fw);
+            bOut.write(outputfile);
+            bOut.close();
+            //write the error file
+            File fErrors = new File(tmpoutputFile.getParent() + File.separator + "errors.xml");
+            FileWriter fwErrors = new FileWriter(fErrors);
+            BufferedWriter bErr = new BufferedWriter(fwErrors);
+            bErr.write(outputErrors);
+            bErr.close();
+        } catch (JDOMException ex) {
+           log.error("processOutputFile", ex);
+        } catch (IOException ex) {
+           log.error("processOutputFile", ex);
+        }
+
+    }
 
 }
