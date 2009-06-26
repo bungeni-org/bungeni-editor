84fdc6b4f1e21ac9ee9a7abc1b86a786913f4950
ashok.hariharan <kohsah@users.noreply.github.com>
2009-06-26 17:45:33 +0000

    Util functions to get expression and cmponent names

---
+++ BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/extutils/CommonANUtils.java
@@ -0,0 +1,50 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.bungeni.extutils;
+
+import java.io.File;
+
+/**
+ *
+ * @author undesa
+ */
+public class CommonANUtils {
+
+
+    public static File getNamedComponentFromFile(String sUrl, String namedComponent) {
+        File fFile = CommonFileFunctions.convertUrlToFile(sUrl);
+        String sComp = fFile.getParent() + File.separator + namedComponent;
+        File fComp = new File(sComp);
+        return fComp;
+
+    }
+
+    public static File getComponentFromFile(String sUrl, String sComponent) {
+        File fFile = CommonFileFunctions.convertUrlToFile(sUrl);
+        String sComp = fFile.getParent() + File.separator + getFilePrefix(fFile.getName()) + "." + sComponent;
+        File fComp = new File(sComp);
+        return fComp;
+    }
+
+    public static String getFilePrefix(String sFile) {
+        int nIndex = sFile.lastIndexOf(".");
+        if (nIndex != -1) {
+            return sFile.substring(0, nIndex);
+        } else
+            return sFile;
+    }
+
+    public static String getFileExt(String sFile) {
+        int nIndex = sFile.lastIndexOf(".");
+        String ssExt = "";
+        if (nIndex != -1) {
+              ssExt =   sFile.substring(nIndex + 1, sFile.length());
+        }
+        return ssExt;
+    }
+
+
+}
