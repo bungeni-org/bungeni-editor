857b5910f85e0ccdb2dcbb913e23dc02a958ff91
ashok.hariharan <kohsah@users.noreply.github.com>
2009-06-26 12:01:14 +0000

    Xml Viewer used by the Editor

---
+++ BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/xml/viewer/BungeniXmlTree.java
@@ -0,0 +1,357 @@
+package org.bungeni.xml.viewer;
+
+import java.io.ByteArrayInputStream;
+import javax.swing.JTree;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.TreeModel;
+import javax.swing.tree.TreeSelectionModel;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+
+
+
+
+/**
+ * Description: The BungeniXmlTree class is an extension of the javax.swing.JTree class.
+ * It behaves in every way like a JTree component, the difference is that additional
+ * methods have been provided to facilitate the parsing of an XML document into a
+ * DOM object and translating that DOM object into a viewable JTree structure.
+ *
+ * Adapted from original source :
+ * Copyright (c) March 2001 Kyle Gabhart
+ * author - Kyle Gabhart
+ *
+ * @author Ashok Hariharan
+ * @version 1.0
+ */
+
+public class BungeniXmlTree extends JTree
+{
+  /**
+   * This member stores the TreeNode object used to create the model for the JTree.
+   * The DefaultMutableTreeNode class is defined in the javax.swing.tree package
+   * and provides a default implementation of the MutableTreeNode interface.
+   */
+  private           DefaultMutableTreeNode      treeNode;
+
+  /**
+   * These three members are a part of the JAXP API and are used to parse the XML
+   * text into a DOM object (of type Document).
+   */
+  private           DocumentBuilderFactory 	dbf;
+  private           DocumentBuilder 		db;
+  private           Document                    doc;
+
+  /**
+   * This constructor builds an BungeniXmlTree object using the XML text
+   * passed in through the constructor.
+   *
+   * @param text A String of XML formatted text
+   *
+   * @exception ParserConfigurationException  This exception is potentially thrown if
+   * the constructor configures the parser improperly.  It won't.
+   */
+  public BungeniXmlTree( String text ) throws ParserConfigurationException
+  {
+	this();
+	refresh( text );
+  } //end BungeniXmlTree( String text )
+
+  /**
+   * This constructor builds the generic portion of the BungeniXmlTree object that is true for
+   * any BungeniXmlTree object.  It includes a default tree model
+   *
+   * @exception ParserConfigurationException  This exception is potentially thrown if
+   * the constructor configures the parser improperly.  It won't.
+   */
+  public BungeniXmlTree() throws ParserConfigurationException
+  {
+      // Initialize the superclass portion of the object
+      super();
+
+      // Set basic properties for the Tree rendering
+      getSelectionModel().setSelectionMode( TreeSelectionModel.SINGLE_TREE_SELECTION );
+      setShowsRootHandles( true );
+      setEditable( false ); // A more advanced version of this tool would allow the Tree to be editable
+
+      // Begin by initializing the object's DOM parsing objects
+      dbf = DocumentBuilderFactory.newInstance();
+      dbf.setValidating( false );
+      db = dbf.newDocumentBuilder();
+
+      // Take the DOM root node and convert it to a Tree model for the JTree
+      setModel( buildWelcomeTree() );
+  } //end BungeniXmlTree()
+
+   /**
+    * This method takes the XML text string passed to it, parses the XML and
+    * constructs the a DefaultTreeModel that can then be used to construct the
+    * graphical tree structure from that data
+    */
+   private DefaultTreeModel buildTree( String text )
+   {
+      DefaultMutableTreeNode    localtreeNode;
+	Node				  newNode;
+
+      // Take the DOM root node and convert it to a Tree model for the JTree
+	newNode = parseXml( text );
+
+	if ( newNode != null )
+	{
+      	localtreeNode = createTreeNode( newNode );
+      	return new DefaultTreeModel( localtreeNode );
+	}
+	else
+		return null;
+   } //end buildTree()
+
+   /**
+    * This method builds a default DefaultTreeModel that can then be used to construct
+    * the graphical tree structure from that data
+    */
+   private DefaultTreeModel buildWelcomeTree()
+   {
+	DefaultMutableTreeNode		root;
+	DefaultMutableTreeNode	 	instructions, openingDoc, editingDoc, savingDoc;
+	DefaultMutableTreeNode		openingDocText, editingDocText, savingDocText;
+	DefaultMutableTreeNode		development, addingFeatures, contactingKyle;
+
+	root = new DefaultMutableTreeNode( "Welcome to XML View 1.0" );
+	instructions = new DefaultMutableTreeNode( "Instructions" );
+	openingDoc = new DefaultMutableTreeNode( "Opening XML Documents" );
+	openingDocText = new DefaultMutableTreeNode( "When invoking the XmlEditor from the command-line, you must specify the filename." );
+	editingDoc = new DefaultMutableTreeNode( "Editing an XML Document" );
+	editingDocText = new DefaultMutableTreeNode( "XML text in the right hand frame can be edited directly.  The \"refresh\" button will rebuild the JTree in the left frame." );
+	savingDoc = new DefaultMutableTreeNode( "Saving an XML Document" );
+	savingDocText = new DefaultMutableTreeNode( "This iteration of the XmlEditor does not provide the ability to save your document.  That will come with the next article." );
+	root.add( instructions );
+	instructions.add( openingDoc );
+	instructions.add( editingDoc );
+	openingDoc.add( openingDocText );
+	editingDoc.add( editingDocText );
+
+	return new DefaultTreeModel( root );
+   } //end buildWelcomeTree()
+
+   /**
+    * This method is public because it will be called by outside classes.
+    * Calling this method will cause the text in the JTextArea to be read,
+    * parsed into a Node object, and used to create a DefaultTreeModel
+    * which is then used to create the graphical tree structure.
+    */
+   public void refresh( String text )
+   {
+	TreeModel		treeMod;
+
+	try
+	{
+		// Take the DOM root node and convert it to a Tree model for the JTree
+		treeMod = buildTree( text );
+		if ( treeMod != null )
+      		setModel( treeMod );
+	}
+	catch ( Throwable ex )
+	{
+		System.out.println ( ex.getMessage() );
+	}
+   } //end refresh()
+
+   /**
+    * This takes a DOM Node and recurses through the children until each one is added
+    * to a DefaultMutableTreeNode. The JTree then uses this object as a tree model.
+    *
+    * @param root org.w3c.Node.Node
+    *
+    * @return Returns a DefaultMutableTreeNode object based on the root Node passed in
+    */
+   private DefaultMutableTreeNode createTreeNode( Node root )
+   {
+      DefaultMutableTreeNode  localtreeNode = null;
+      String                  type, name, value;
+      NamedNodeMap            attribs;
+      Node                    attribNode;
+
+      // Get data from root node
+      type = getNodeType( root );
+      name = root.getNodeName();
+      value = root.getNodeValue();
+
+      // Special case for TEXT_NODE
+      localtreeNode = new DefaultMutableTreeNode( root.getNodeType() == Node.TEXT_NODE ? value : name );
+
+      // Display the attributes if there are any
+      attribs = root.getAttributes();
+      if( attribs != null )
+      {
+         for( int i = 0; i < attribs.getLength(); i++ )
+         {
+            attribNode = attribs.item(i);
+            name = attribNode.getNodeName().trim();
+            value = attribNode.getNodeValue().trim();
+
+            if ( value != null )
+            {
+               if ( value.length() > 0 )
+               {
+                  localtreeNode.add( new DefaultMutableTreeNode( "[Attribute] --> " + name + "=\"" + value + "\"" ) );
+               } //end if ( value.length() > 0 )
+            } //end if ( value != null )
+         } //end for( int i = 0; i < attribs.getLength(); i++ )
+      } //end if( attribs != null )
+
+      // Recurse children nodes if any exist
+      if( root.hasChildNodes() )
+      {
+         NodeList             children;
+         int                  numChildren;
+         Node                 node;
+         String               data;
+
+         children = root.getChildNodes();
+         // Only recurse if Child Nodes are non-null
+         if( children != null )
+         {
+            numChildren = children.getLength();
+
+            for (int i=0; i < numChildren; i++)
+            {
+               node = children.item(i);
+               if( node != null )
+               {
+                  // A special case could be made for each Node type.
+                  if( node.getNodeType() == Node.ELEMENT_NODE )
+                  {
+                     localtreeNode.add( createTreeNode(node) );
+                  } //end if( node.getNodeType() == Node.ELEMENT_NODE )
+
+                  data = node.getNodeValue();
+
+                  if( data != null )
+                  {
+                     data = data.trim();
+                     if ( !data.equals("\n") && !data.equals("\r\n") && data.length() > 0 )
+                     {
+                        localtreeNode.add(createTreeNode(node));
+                     } //end if ( !data.equals("\n") && !data.equals("\r\n") && data.length() > 0 )
+                  } //end if( data != null )
+               } //end if( node != null )
+            } //end for (int i=0; i < numChildren; i++)
+         } //end if( children != null )
+      } //end if( root.hasChildNodes() )
+      return localtreeNode;
+   } //end createTreeNode( Node root )
+
+   /**
+    * This method returns a string representing the type of node passed in.
+    *
+    * @param node org.w3c.Node.Node
+    *
+    * @return Returns a String representing the node type
+    */
+   private String getNodeType( Node node )
+   {
+      String type;
+
+      switch( node.getNodeType() )
+      {
+         case Node.ELEMENT_NODE:
+         {
+            type = "Element";
+            break;
+         }
+         case Node.ATTRIBUTE_NODE:
+         {
+            type = "Attribute";
+            break;
+         }
+         case Node.TEXT_NODE:
+         {
+            type = "Text";
+            break;
+         }
+         case Node.CDATA_SECTION_NODE:
+         {
+            type = "CData section";
+            break;
+         }
+         case Node.ENTITY_REFERENCE_NODE:
+         {
+            type = "Entity reference";
+            break;
+         }
+         case Node.ENTITY_NODE:
+         {
+            type = "Entity";
+            break;
+         }
+         case Node.PROCESSING_INSTRUCTION_NODE:
+         {
+            type = "Processing instruction";
+            break;
+         }
+         case Node.COMMENT_NODE:
+         {
+            type = "Comment";
+            break;
+         }
+         case Node.DOCUMENT_NODE:
+         {
+            type = "Document";
+            break;
+         }
+         case Node.DOCUMENT_TYPE_NODE:
+         {
+            type = "Document type";
+            break;
+         }
+         case Node.DOCUMENT_FRAGMENT_NODE:
+         {
+            type = "Document fragment";
+            break;
+         }
+         case Node.NOTATION_NODE:
+         {
+            type = "Notation";
+            break;
+         }
+         default:
+         {
+            type = "???";
+            break;
+         }
+      }// end switch( node.getNodeType() )
+      return type;
+   } //end getNodeType()
+
+   /**
+    * This method performs the actual parsing of the XML text
+    *
+    * @param text A String representing an XML document
+    * @return Returns an org.w3c.Node.Node object
+    */
+   private Node parseXml( String xml )
+   {
+	ByteArrayInputStream	byteStream;
+
+	byteStream = new ByteArrayInputStream( xml.getBytes() );
+
+      try
+      {
+         doc = db.parse( byteStream );
+      }
+      catch ( Exception e )
+      {
+        System.out.println( e.getMessage() );
+	  return null;
+      }
+      return ( Node )doc.getDocumentElement();
+   } //end parseXml
+
+} //end class BungeniXmlTree
---
+++ BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/xml/viewer/BungeniXmlViewer.java
@@ -0,0 +1,258 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.bungeni.xml.viewer;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+import javax.swing.JTextArea;
+import javax.swing.tree.TreeSelectionModel;
+import javax.xml.parsers.ParserConfigurationException;
+import org.apache.log4j.Logger;
+
+/**
+ * Description: ...
+ *
+ * Copyright (c) March 2001 Kyle Gabhart
+ * @author Kyle Gabhart
+ * @version 1.0
+ */
+public class BungeniXmlViewer extends JFrame implements ActionListener {
+   private static org.apache.log4j.Logger log = Logger.getLogger(BungeniXmlViewer.class.getName());
+
+    private static BungeniXmlViewer xmlViewer = null;
+    // This is the BungeniXmlTree object which displays the XML in a JTree
+    private BungeniXmlTree BungeniXmlTree;
+    // This is the textArea object that will display the raw XML text
+    private JTextArea textArea;
+    // One JScrollPane is the container for the JTree, the other is for the textArea
+    private JScrollPane jScroll,  jScrollRt;
+    // This JSplitPane is the container for the two JScrollPanes
+    private JSplitPane splitPane;
+    // This JButton handles the tree Refresh feature
+    private JButton refreshButton;
+    // This Listener allows the frame's close button to work properly
+    private WindowListener winClosing;
+
+    // These two constants set the width and height of the frame
+    private static final int FRAME_WIDTH = 600;
+    private static final int FRAME_HEIGHT = 450;
+
+    public static BungeniXmlViewer getInstance(String title, ArrayList<String> xmlText) throws ParserConfigurationException {
+        if (xmlViewer == null) {
+            return new BungeniXmlViewer(title, xmlText);
+        } else {
+            return xmlViewer.updateContent(title, xmlText);
+        }
+    }
+
+    public static void launchXmlViewer(String title, File fFile) throws ParserConfigurationException {
+        BufferedReader reader = null;
+        try {
+            // The file will have to be re-read when the Document object is parsed
+            reader = new BufferedReader(new FileReader(fFile));
+            ArrayList<String> xmlText = new ArrayList<String>();
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                xmlText.add(line);
+            } //end while ( ( line = reader.readLine() ) != null )
+            // The file will have to be re-read when the Document object is parsed
+            reader.close();
+            BungeniXmlViewer.getInstance(title, xmlText);
+        } catch (IOException ex) {
+            log.error("launchXmlViewer" , ex);
+        } finally {
+            try {
+                if (reader != null) reader.close();
+            } catch (IOException ex) {
+                    log.error("launchXmlViewer" , ex);
+            }
+        }
+
+    }
+
+    private BungeniXmlViewer updateContent(String title, ArrayList<String> xmlText) {
+        setTitle(title);
+        textArea.setText((String) xmlText.get(0) + "\n");
+        for (int i = 1; i < xmlText.size(); i++) {
+            textArea.append((String) xmlText.get(i) + "\n");
+        }
+        BungeniXmlTree.refresh(textArea.getText());
+        return this;
+    }
+
+    /**
+     * This constructor passes the graphical construction off to the overloaded constructor
+     * and then handles the processing of the XML text
+     */
+    public BungeniXmlViewer(String title, ArrayList<String> xmlText) throws ParserConfigurationException {
+        this(title);
+        updateContent(title, xmlText);
+    } //end BungeniXmlViewer( String title, String xml )
+
+    /**
+     * This constructor builds a frame containing a JSplitPane, which in turn contains two JScrollPanes.
+     * One of the panes contains an BungeniXmlTree object and the other contains a JTextArea object.
+     */
+    public BungeniXmlViewer(String title) throws ParserConfigurationException {
+        // This builds the JFrame portion of the object
+        super(title);
+
+        Toolkit toolkit;
+        Dimension dim, minimumSize;
+        int screenHeight, screenWidth;
+
+        // Initialize basic layout properties
+        setBackground(Color.lightGray);
+        getContentPane().setLayout(new BorderLayout());
+
+        // Set the frame's display to be WIDTH x HEIGHT in the middle of the screen
+        toolkit = Toolkit.getDefaultToolkit();
+        dim = toolkit.getScreenSize();
+        screenHeight = dim.height;
+        screenWidth = dim.width;
+        setBounds((screenWidth - FRAME_WIDTH) / 2, (screenHeight - FRAME_HEIGHT) / 2, FRAME_WIDTH, FRAME_HEIGHT);
+
+        // Create the refresh button object
+        refreshButton = new JButton("Refresh");
+        refreshButton.setBorder(BorderFactory.createRaisedBevelBorder());
+        refreshButton.addActionListener(this);
+
+        // Add the button to the frame
+        getContentPane().add(refreshButton, BorderLayout.NORTH);
+
+        // Create two JScrollPane objects
+        jScroll = new JScrollPane();
+        jScrollRt = new JScrollPane();
+
+        // First, create the JTextArea:
+        // Create the JTextArea and add it to the right hand JScroll
+        textArea = new JTextArea(150, 150);
+        jScrollRt.getViewport().add(textArea);
+
+        // Next, create the BungeniXmlTree
+        BungeniXmlTree = new BungeniXmlTree();
+        BungeniXmlTree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
+        BungeniXmlTree.setShowsRootHandles(true);
+
+        // A more advanced version of this tool would allow the JTree to be editable
+        BungeniXmlTree.setEditable(false);
+
+        // Wrap the JTree in a JScroll so that we can scroll it in the JSplitPane.
+        jScroll.getViewport().add(BungeniXmlTree);
+
+        // Create the JSplitPane and add the two JScroll objects
+        splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, jScroll, jScrollRt);
+        splitPane.setOneTouchExpandable(true);
+        splitPane.setDividerLocation(300);
+
+        // Provide minimum sizes for the two components in the split pane
+        minimumSize = new Dimension(200, 150);
+        jScroll.setMinimumSize(minimumSize);
+        jScrollRt.setMinimumSize(minimumSize);
+
+        // Provide a preferred size for the split pane
+        splitPane.setPreferredSize(new Dimension(400, 300));
+
+        // Add the split pane to the frame
+        getContentPane().add(splitPane, BorderLayout.CENTER);
+
+        //Put the final touches to the JFrame object
+        validate();
+        setAlwaysOnTop(true);
+        toFront();
+        setVisible(true);
+
+    // Add a WindowListener so that we can close the window
+    //  winClosing = new WindowAdapter()
+    //  {
+    //     public void windowClosing(WindowEvent e)
+    //     {
+    //        exit();
+    //     }
+    //  };
+    //  addWindowListener(winClosing);
+    } //end BungeniXmlViewer()
+
+    /**
+     * When a user event occurs, this method is called.  If the action performed was a click
+     * of the "Refresh" button, then the BungeniXmlTree object is updated using the current XML text
+     * contained in the JTextArea
+     */
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getActionCommand().equals("Refresh")) {
+            BungeniXmlTree.refresh(textArea.getText());
+        }
+    } //end actionPerformed()
+
+    // Program execution begins here.  An XML file (*.xml) must be passed into the method
+    public static void main(String[] args) {
+        String fileName = "";
+        BufferedReader reader;
+        String line;
+        ArrayList<String> xmlText = null;
+        BungeniXmlViewer BungeniXmlViewer;
+
+        // Build a Document object based on the specified XML file
+        try {
+            if (args.length > 0) {
+                fileName = args[0];
+
+                if (fileName.substring(fileName.indexOf('.')).equals(".xml")) {
+                    reader = new BufferedReader(new FileReader(fileName));
+                    xmlText = new ArrayList<String>();
+
+                    while ((line = reader.readLine()) != null) {
+                        xmlText.add(line);
+                    } //end while ( ( line = reader.readLine() ) != null )
+
+                    // The file will have to be re-read when the Document object is parsed
+                    reader.close();
+
+                    // Construct the GUI components and pass a reference to the XML root node
+                    BungeniXmlViewer = new BungeniXmlViewer("BungeniXmlViewer 1.0", xmlText);
+                } else {
+                    help();
+                } //end if ( fileName.substring( fileName.indexOf( '.' ) ).equals( ".xml" ) )
+            } else {
+                BungeniXmlViewer = new BungeniXmlViewer("BungeniXmlViewer 1.0");
+            } //end if( args.length > 0 )
+        } catch (FileNotFoundException fnfEx) {
+            System.out.println(fileName + " was not found.");
+            exit();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            exit();
+        }// end try/catch
+    }// end main()
+
+    // A common source of operating instructions
+    private static void help() {
+        System.out.println("\nUsage: java BungeniXmlViewer filename.xml");
+        System.exit(0);
+    } //end help()
+
+    // A common point of exit
+    private static void exit() {
+        System.out.println("\nThank you for using BungeniXmlViewer 1.0");
+        System.exit(0);
+    } //end exit()
+} //end class BungeniXmlViewer
