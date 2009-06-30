ea4c4bcfe67dcb8a32114c7eebbeecb4a9907288
ashok.hariharan <kohsah@users.noreply.github.com>
2009-06-30 13:08:39 +0000

    cleaning up

--- BungeniEditor/trunk/BungeniEditorClient/dist/runEditor.bat
+++
@@ -1,4 +0,0 @@
-set JAVA_HOME=e:\Java\jdk1.6.0
-set LOG4J_PROPS=log4jdev.properties
-set EDITOR_MEM=128m
-%JAVA_HOME%\bin\java -Dlog4j.ignoreTCL -Dlog4j.configuration=%LOG4J_PROPS% -Xmx%EDITOR_MEM% -jar BungeniEditorClient.jar
