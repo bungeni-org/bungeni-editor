91caed3cd31ab5f2b63945fc0f8bc01fdb6d92ed
ashok.hariharan <kohsah@users.noreply.github.com>
2009-06-30 13:04:24 +0000

    removing plugins folder

--- BungeniEditor/trunk/BungeniEditorClient/dist/plugins/convert_to_plain/getRequired.sh
+++
@@ -1,6 +0,0 @@
-cp /home/undesa/Projects/Bungeni/BungeniOdfDom/dist/bungeniodfdom.jar .
-cp /home/undesa/Projects/Bungeni/BungeniSectionPlain/dist/BungeniSectionPlain.jar .
-cp /home/undesa/Projects/Bungeni/external_libs/xerces/xerces2.9.1/xercesImpl.jar .
-cp /home/undesa/Projects/Bungeni/external_libs/apache-log4j/log4j.jar .
-cp /home/undesa/Projects/Bungeni/external_libs/odfdom/odfdom.jar .
-cp /home/undesa/Projects/Bungeni/EditorPluginInterface/dist/editorplugininterface.jar .
