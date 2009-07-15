7395cfc75c9ada1e4b2fe3c39bb99f5483aad316
ashok.hariharan <kohsah@users.noreply.github.com>
2009-07-15 09:05:46 +0000

    Reformatted / Added documentation

--- OpenOfficeHelper/trunk/OOoBungeniLib/src/org/bungeni/ooo/ooDocMetadata.java
+++ OpenOfficeHelper/trunk/OOoBungeniLib/src/org/bungeni/ooo/ooDocMetadata.java
@@ -1,58 +1,56 @@
-/*
- * ooDocMetadata.java
- *
- * Created on September 27, 2007, 10:20 AM
- *
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
- */
-
 package org.bungeni.ooo;
 
+//~--- JDK imports ------------------------------------------------------------
+
 import com.sun.star.beans.Property;
+
 import java.util.ArrayList;
 
 /**
- *
- * @author Administrator
+ * Helper class to manage OpenOffice documetn metadata via UNO
+ * @author Ashok Hariharan
  */
 public class ooDocMetadata {
-    private OOComponentHelper ooDocument;
-   private static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(ooDocMetadata.class.getName());
-  
+    private static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(ooDocMetadata.class.getName());
+    private OOComponentHelper              ooDocument;
+
     /** Creates a new instance of ooDocMetadata */
     public ooDocMetadata(OOComponentHelper docHandle) {
-        ooDocument  = docHandle;
+        ooDocument = docHandle;
     }
-    
+
     public void setOODocument(OOComponentHelper ooDoc) {
         this.ooDocument = ooDoc;
     }
-    public void AddProperty (String propertyName, String propertyValue ){
+
+    public void AddProperty(String propertyName, String propertyValue) {
         if (!PropertyExists(propertyName)) {
-        ooDocument.addProperty(propertyName, propertyValue);
+            ooDocument.addProperty(propertyName, propertyValue);
         } else {
             SetProperty(propertyName, propertyValue);
         }
     }
-    
-    public void SetProperty (String propertyName, String propertyValue ) {
+
+    public void SetProperty(String propertyName, String propertyValue) {
         ooDocument.setPropertyValue(propertyName, propertyValue);
     }
-    
-    public boolean PropertyExists (String propertyName) {
+
+    public boolean PropertyExists(String propertyName) {
         return ooDocument.propertyExists(propertyName);
     }
-    
-    public String GetProperty (String propertyName)  {
+
+    public String GetProperty(String propertyName) {
         String propertyValue = null;
-       try {
-        if (PropertyExists (propertyName))
+
+        try {
+            if (PropertyExists(propertyName)) {
                 propertyValue = ooDocument.getPropertyValue(propertyName);
-       } finally {
-       return propertyValue;
-       }
+            }
+        } finally {
+            return propertyValue;
+        }
     }
+
     /**
      * Returns document metadata properties by type
      * Properties are 'typed' based on a prefix e.g. rf: is an internal reference prefix
@@ -60,21 +58,24 @@ public class ooDocMetadata {
      * @param docMetadataType Type filter prefix (without ":" )
      * @return
      */
-    public static ArrayList<ooDocMetadataFieldSet> getMetadataObjectsByType (OOComponentHelper ooDoc, String docMetadataType) {
+    public static ArrayList<ooDocMetadataFieldSet> getMetadataObjectsByType(OOComponentHelper ooDoc,
+            String docMetadataType) {
         ArrayList<ooDocMetadataFieldSet> matchedProperties = new ArrayList<ooDocMetadataFieldSet>(0);
-        Property[] docProps = ooDoc.getDocumentProperties();
+        Property[]                       docProps          = ooDoc.getDocumentProperties();
+
         for (Property prop : docProps) {
-            if (prop.Name.startsWith(docMetadataType)){
+            if (prop.Name.startsWith(docMetadataType)) {
                 try {
-                    String value = ooDoc.getPropertyValue(prop.Name);
-                    ooDocMetadataFieldSet fld = new ooDocMetadataFieldSet(prop.Name, value);
+                    String                value = ooDoc.getPropertyValue(prop.Name);
+                    ooDocMetadataFieldSet fld   = new ooDocMetadataFieldSet(prop.Name, value);
+
                     matchedProperties.add(fld);
                 } catch (Exception ex) {
                     log.error("getMetadataObjectsByType : " + ex.getMessage());
                 }
             }
         }
+
         return matchedProperties;
     }
-    
 }
