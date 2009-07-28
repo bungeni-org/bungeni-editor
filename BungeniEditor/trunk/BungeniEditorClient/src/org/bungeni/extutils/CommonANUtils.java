5857e40c25257953f0caa180b3e12612c83a216e
ashok.hariharan <kohsah@users.noreply.github.com>
2009-07-28 14:37:47 +0000

    AN helper functions

--- BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/extutils/CommonANUtils.java
+++ BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/extutils/CommonANUtils.java
@@ -1,9 +1,6 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package org.bungeni.extutils;
+
+//~--- JDK imports ------------------------------------------------------------
 
 import java.io.File;
 
@@ -12,39 +9,40 @@ import java.io.File;
  * @author undesa
  */
 public class CommonANUtils {
-
-
     public static File getNamedComponentFromFile(String sUrl, String namedComponent) {
-        File fFile = CommonFileFunctions.convertUrlToFile(sUrl);
+        File   fFile = CommonFileFunctions.convertUrlToFile(sUrl);
         String sComp = fFile.getParent() + File.separator + namedComponent;
-        File fComp = new File(sComp);
-        return fComp;
+        File   fComp = new File(sComp);
 
+        return fComp;
     }
 
     public static File getComponentFromFile(String sUrl, String sComponent) {
-        File fFile = CommonFileFunctions.convertUrlToFile(sUrl);
+        File   fFile = CommonFileFunctions.convertUrlToFile(sUrl);
         String sComp = fFile.getParent() + File.separator + getFilePrefix(fFile.getName()) + "." + sComponent;
-        File fComp = new File(sComp);
+        File   fComp = new File(sComp);
+
         return fComp;
     }
 
     public static String getFilePrefix(String sFile) {
         int nIndex = sFile.lastIndexOf(".");
+
         if (nIndex != -1) {
             return sFile.substring(0, nIndex);
-        } else
+        } else {
             return sFile;
+        }
     }
 
     public static String getFileExt(String sFile) {
-        int nIndex = sFile.lastIndexOf(".");
-        String ssExt = "";
+        int    nIndex = sFile.lastIndexOf(".");
+        String ssExt  = "";
+
         if (nIndex != -1) {
-              ssExt =   sFile.substring(nIndex + 1, sFile.length());
+            ssExt = sFile.substring(nIndex + 1, sFile.length());
         }
+
         return ssExt;
     }
-
-
 }
