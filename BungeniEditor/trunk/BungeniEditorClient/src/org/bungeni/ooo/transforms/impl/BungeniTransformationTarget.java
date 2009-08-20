6f870b969941997c754d6408a22391866a6e1601
ashok.hariharan <kohsah@users.noreply.github.com>
2009-08-20 15:55:14 +0000

    Improved readability

--- BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/ooo/transforms/impl/BungeniTransformationTarget.java
+++ BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/ooo/transforms/impl/BungeniTransformationTarget.java
@@ -1,36 +1,27 @@
-/*
- * BungeniTransformationTarget.java
- *
- * Created on June 3, 2008, 4:30 PM
- *
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
- */
-
 package org.bungeni.ooo.transforms.impl;
 
 /**
  *
- * @author Administrator
+ * @author Ashok Hariharan
  */
 public class BungeniTransformationTarget {
-       public String targetName = "";
-       public String targetDescription = "";
-       public String targetClass = "";
-       public String targetExt = "";
+    public String targetClass       = "";
+    public String targetDescription = "";
+    public String targetExt         = "";
+    public String targetName        = "";
+
     /** Creates a new instance of BungeniTransformationTarget */
-    public BungeniTransformationTarget() {
+    public BungeniTransformationTarget() {}
+
+    public BungeniTransformationTarget(String name, String desc, String ext, String sClass) {
+        this.targetName        = name;
+        this.targetDescription = desc;
+        this.targetExt         = ext;
+        this.targetClass       = sClass;
     }
-        
-     public BungeniTransformationTarget (String name, String desc, String ext, String sClass ) {
-            this.targetName = name;
-            this.targetDescription = desc;
-            this.targetExt = ext;
-            this.targetClass = sClass;
-     }
 
     @Override
     public String toString() {
-            return this.targetDescription;
-   } 
+        return this.targetDescription;
+    }
 }
