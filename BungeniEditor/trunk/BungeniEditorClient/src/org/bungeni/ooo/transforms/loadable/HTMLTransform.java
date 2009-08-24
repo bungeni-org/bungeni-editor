4020d8ee5a06746fabd2c7cfeebe04b017b315f9
ashok.hariharan <kohsah@users.noreply.github.com>
2009-08-24 10:17:44 +0000

    Run pdf and html transform in background thread

--- BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/ooo/transforms/loadable/HTMLTransform.java
+++ BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/ooo/transforms/loadable/HTMLTransform.java
@@ -9,9 +9,12 @@ import org.bungeni.ooo.transforms.impl.BungeniDocTransform;
 
 import com.sun.star.beans.PropertyValue;
 import com.sun.star.frame.XStorable;
-import com.sun.star.io.IOException;
 
+import java.io.File;
 import java.util.ArrayList;
+import org.bungeni.extutils.CommonFileFunctions;
+import org.bungeni.extutils.MessageBox;
+import org.bungeni.ooo.utils.CommonExceptionUtils;
 
 /**
  *
@@ -19,7 +22,8 @@ import java.util.ArrayList;
  */
 public class HTMLTransform extends BungeniDocTransform {
     private static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(HTMLTransform.class.getName());
-
+    private static String EXPORT_EXT = "html";
+    private String EXPORT_OUTPUT_FILE = "";
     /** Creates a new instance of HTMLTransform */
     public HTMLTransform() {
         super();
@@ -29,17 +33,50 @@ public class HTMLTransform extends BungeniDocTransform {
         boolean bState = false;
 
         try {
-            XStorable docStore  = ooDocument.getStorable();
-            String    urlString = (String) getParams().get("StoreToUrl");
 
-            docStore.storeToURL(urlString, getTransformProps().toArray(new PropertyValue[getTransformProps().size()]));
-            bState = true;
-        } catch (IOException ex) {
+            // 1 - query interface the document for the storable url
+            File fopenDocumentFile = null;
+
+            if (ooDocument.isDocumentOnDisk()) {
+                String sDocUrl = ooDocument.getDocumentURL();
+
+                // set the path to the output xml file
+                fopenDocumentFile = CommonFileFunctions.convertUrlToFile(sDocUrl);
+
+                String fullFileName = fopenDocumentFile.getName();
+                String ext          = fullFileName.substring(fullFileName.lastIndexOf(".") + 1, fullFileName.length());
+                String pref         = fullFileName.substring(0, fullFileName.lastIndexOf("."));
+
+                EXPORT_OUTPUT_FILE = fopenDocumentFile.getParentFile().getPath() + File.separator + pref + "." + EXPORT_EXT;
+
+                // get temporary output file
+                OutputFile            outpdfFile   = new OutputFile(EXPORT_OUTPUT_FILE, EXPORT_EXT);
+                String uriOutFile = outpdfFile.getFullFile().toURI().toString();
+                try {
+                    XStorable       docStore  = ooDocument.getStorable();
+                    //String          urlString = (String) getParams().get("StoreToUrl");
+                    PropertyValue[] props     = getTransformProps().toArray(new PropertyValue[getTransformProps().size()]);
+
+                    docStore.storeToURL(uriOutFile, props);
+                    bState = true;
+                } catch (com.sun.star.io.IOException ex) {
+                    log.error("transform : " + ex.getMessage());
+                    log.error("transform : " + CommonExceptionUtils.getStackTrace(ex));
+                }
+
+
+            } else {
+                MessageBox.OK("Please save the document before trying to transform it!");
+                bState = false;
+            }
+        } catch (Exception ex) {
             log.error("transform : " + ex.getMessage());
+            log.error("transform : " + CommonExceptionUtils.getStackTrace(ex));
         }
 
         return bState;
     }
+
 
     private ArrayList<PropertyValue> getTransformProps() {
         ArrayList<PropertyValue> props = new ArrayList<PropertyValue>();
