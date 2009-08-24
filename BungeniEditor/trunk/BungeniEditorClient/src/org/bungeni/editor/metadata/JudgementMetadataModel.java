366037f2e452e0b7a62d92cfe39574aade526644
ashok.hariharan <kohsah@users.noreply.github.com>
2009-08-24 21:29:57 +0000

    Updated judgement metadata model

--- BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/editor/metadata/JudgementMetadataModel.java
+++ BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/editor/metadata/JudgementMetadataModel.java
@@ -1,9 +1,6 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package org.bungeni.editor.metadata;
+
+//~--- non-JDK imports --------------------------------------------------------
 
 import org.bungeni.ooo.OOComponentHelper;
 import org.bungeni.ooo.ooDocMetadata;
@@ -13,24 +10,17 @@ import org.bungeni.ooo.ooDocMetadata;
  * @author undesa
  */
 public class JudgementMetadataModel extends BaseEditorDocMetaModel {
-    public JudgementMetadataModel(){
+    public static final String[] GROUPED_METADATA = { "BungeniJudgeName:", "BungeniPartyName:" };
+    public static final String[] THIS_METAMODEL   = { "BungeniJudgementNo", "BungeniCaseNo", "BungeniJudgementDate" };
+
+    public JudgementMetadataModel() {
         super();
     }
 
-    public static final String[] THIS_METAMODEL = {
-        "BungeniJudgementNo",
-        "BungeniCaseNo",
-        "BungeniJudgementDate"
-    };
-
-    public static final String[] GROUPED_METADATA = {
-        "BungeniJudgeName:",
-        "BungeniPartyName:"
-    };
-    
     @Override
-    public void setup(){
+    public void setup() {
         super.setup();
+
         for (String sMeta : THIS_METAMODEL) {
             this.docMeta.put(sMeta, "");
         }
@@ -38,16 +28,20 @@ public class JudgementMetadataModel extends BaseEditorDocMetaModel {
 
     @Override
     public void saveModel(OOComponentHelper ooDocument) {
-         ooDocMetadata docM = new ooDocMetadata(ooDocument);
-         for (String sMeta : THIS_METAMODEL) {
-             docM.AddProperty(sMeta, docMeta.get(sMeta));
-         }
-         //now process grouped metadata
-         //we process all metadata starting with the grouped metadata filter :
-         for (String sGmeta : docMeta.keySet()) {
-             for (String sGfilter : GROUPED_METADATA) {
+        ooDocMetadata docM = new ooDocMetadata(ooDocument);
 
-             }
-         }
+        for (String sMeta : THIS_METAMODEL) {
+            docM.AddProperty(sMeta, docMeta.get(sMeta));
+        }
+
+        // now process grouped metadata
+        // we process all metadata starting with the grouped metadata filter :
+        for (String sGmeta : docMeta.keySet()) {
+            for (String sGfilter : GROUPED_METADATA) {
+                if (sGmeta.startsWith(sGfilter)) {
+                    docM.AddProperty(sGmeta, docMeta.get(sGmeta));
+                }
+            }
+        }
     }
 }
