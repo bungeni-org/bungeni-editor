0640a894906bf1566576bd38f026898856ec43fb
ashok.hariharan <kohsah@users.noreply.github.com>
2009-09-07 10:17:57 +0000

    Added ServerURI api

--- utils/BungeniExistClient/src/org/bungeni/editor/exist/client/XmlDbClientConfig.java
+++ utils/BungeniExistClient/src/org/bungeni/editor/exist/client/XmlDbClientConfig.java
@@ -9,4 +9,9 @@ public class XmlDbClientConfig {
     public static String SERVER_NAME ;
     public static String SERVER_PORT;
     public static String URL_PREFIX;
+    public static String PROTOCOL ;
+
+    public static String ServerURI() {
+        return PROTOCOL + "://" + SERVER_NAME + ":" + SERVER_PORT ;
+    }
 }
