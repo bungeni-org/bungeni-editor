30e5ec7f6a0bd2323b1df95a766449399d2eaaad
ashok.hariharan <kohsah@users.noreply.github.com>
2009-09-25 13:51:18 +0000

    Added query to retrieve active doc types

--- BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/db/SettingsQueryFactory.java
+++ BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/db/SettingsQueryFactory.java
@@ -249,6 +249,12 @@ public class SettingsQueryFactory {
         return query;
     }
 
+    public static String Q_FETCH_ALL_ACTIVE_DOCUMENT_TYPES() {
+        String query = "Select doc_type, description, template_path from DOCUMENT_TYPES where state = 1";
+        return query;
+    }
+
+
     public static String Q_FETCH_DOCUMENT_TYPE_BY_NAME(String docType) {
         String query =
             "Select doc_type, description, template_path, metadata_model_editor, metadata_editor_title, work_uri, exp_uri, file_name_scheme from DOCUMENT_TYPES ";
