9e6b268952bc26e905e609b897807677b4157443
ashok.hariharan <kohsah@users.noreply.github.com>
2010-03-01 09:27:41 +0000

    Adding interfaces to panel and container classes

---
+++ plugins/TrackChangesManager/src/org/bungeni/trackchanges/IChangesContainer.java
@@ -0,0 +1,10 @@
+package org.bungeni.trackchanges;
+
+/**
+ * Interface class for tabs container
+ * @author Ashok Hariharan
+ */
+public interface IChangesContainer {
+    public void updatePanels();
+    public void updateCurrentPanel();
+}
---
+++ plugins/TrackChangesManager/src/org/bungeni/trackchanges/IChangesPanel.java
@@ -0,0 +1,9 @@
+package org.bungeni.trackchanges;
+
+/**
+ * Interface class for tabs panel
+ * @author Ashok Hariharan
+ */
+public interface IChangesPanel {
+    public void updatePanel();
+}
