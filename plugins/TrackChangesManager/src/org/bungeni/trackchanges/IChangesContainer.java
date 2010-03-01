2a9ff9f61e6b0a6584d98474cfadaac9317251a7
ashok.hariharan <kohsah@users.noreply.github.com>
2010-03-01 13:14:27 +0000

    Added message passing support API

--- plugins/TrackChangesManager/src/org/bungeni/trackchanges/IChangesContainer.java
+++ plugins/TrackChangesManager/src/org/bungeni/trackchanges/IChangesContainer.java
@@ -1,10 +1,12 @@
 package org.bungeni.trackchanges;
 
+import java.util.HashMap;
+
 /**
  * Interface class for tabs container
  * @author Ashok Hariharan
  */
 public interface IChangesContainer {
-    public void updatePanels();
-    public void updateCurrentPanel();
+    public void updatePanels(HashMap<String, Object> infomap);
+    public void updateCurrentPanel(HashMap<String,Object> infomap);
 }
--- plugins/TrackChangesManager/src/org/bungeni/trackchanges/IChangesPanel.java
+++ plugins/TrackChangesManager/src/org/bungeni/trackchanges/IChangesPanel.java
@@ -1,9 +1,11 @@
 package org.bungeni.trackchanges;
 
+import java.util.HashMap;
+
 /**
  * Interface class for tabs panel
  * @author Ashok Hariharan
  */
 public interface IChangesPanel {
-    public void updatePanel();
+    public void updatePanel(HashMap<String,Object> infomap);
 }
