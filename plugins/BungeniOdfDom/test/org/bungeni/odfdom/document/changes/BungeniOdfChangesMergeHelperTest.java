6139e975492804c863b7820bfa70a9ba7f209e0a
ashok.hariharan <kohsah@users.noreply.github.com>
2010-03-08 22:19:19 +0000

    Unit test working with refactored test xml . TODO with ODF

--- plugins/BungeniOdfDom/test/org/bungeni/odfdom/document/changes/BungeniOdfChangesMergeHelperTest.java
+++ plugins/BungeniOdfDom/test/org/bungeni/odfdom/document/changes/BungeniOdfChangesMergeHelperTest.java
@@ -1,14 +1,19 @@
 package org.bungeni.odfdom.document.changes;
 
-import java.io.File;
 import java.io.IOException;
+import java.io.StringWriter;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 import org.apache.log4j.Logger;
 import org.bungeni.odfdom.document.BungeniOdfDocumentHelper;
@@ -19,8 +24,6 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import static org.junit.Assert.*;
 import org.xml.sax.SAXException;
 
 /**
@@ -76,8 +79,45 @@ public class BungeniOdfChangesMergeHelperTest {
     public void testMergeChanges() {
         try {
             XPath xpath = XPathFactory.newInstance().newXPath();
-            String xPathExpr = "//change-start[@id='2']/following::text()[not(preceding::change-end[id='2'])]";
-            NodeList foundNodes = (NodeList) xpath.evaluate(xPathExpr, wdoc, XPathConstants.NODESET);
+            String xPathExpr = "//change-start[@id='2']";
+            Node startNode = (Node)  xpath.evaluate(xPathExpr, wdoc, XPathConstants.NODE);
+            Node endNode = (Node) xpath.evaluate("//change-end[@id='2']", wdoc, XPathConstants.NODE);
+            Node nextSibling = startNode.getNextSibling();
+            ArrayList<Node> clonedNodes = new ArrayList<Node>(0);
+            while (!nextSibling.isSameNode(endNode)) {
+                Node clonedNode = nextSibling.cloneNode(true);
+                clonedNodes.add(clonedNode);
+                System.out.println(nextSibling.getNodeName());
+                nextSibling = nextSibling.getNextSibling();
+            }
+
+            nextSibling = startNode.getNextSibling();
+            while(!nextSibling.isSameNode(endNode)) {
+                nextSibling.getParentNode().removeChild(nextSibling);
+                nextSibling = startNode.getNextSibling();
+            }
+            startNode.getParentNode().removeChild(startNode);
+            endNode.getParentNode().removeChild(endNode);
+
+            /*
+            String xPath2 = "//change-end[@id='1']";
+            Node endNode = (Node) xpath.evaluate(xPath2, wdoc, XPathConstants.NODE);
+          */
+            Node endNodeOne = (Node) xpath.evaluate("//change-end[@id='1']", wdoc, XPathConstants.NODE);
+            for (Node node : clonedNodes) {
+                endNodeOne.getParentNode().insertBefore(node, endNodeOne);
+            }
+
+            Transformer transformer = TransformerFactory.newInstance().newTransformer();
+transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+
+//initialize StreamResult with File object to save to file
+StreamResult result = new StreamResult(new StringWriter());
+DOMSource source = new DOMSource(wdoc);
+transformer.transform(source, result);
+
+String xmlString = result.getWriter().toString();
+System.out.println(xmlString);
             //System.out.println(foundNode.getNodeName());
             /*
             BungeniOdfChangesMergeHelper instance = docHelper.getChangesHelper().getChangesMergeHelper();
@@ -85,7 +125,7 @@ public class BungeniOdfChangesMergeHelperTest {
             assertEquals(true, result);
              *
              */
-        } catch (XPathExpressionException ex) {
+        } catch (Exception ex) {
             java.util.logging.Logger.getLogger(BungeniOdfChangesMergeHelperTest.class.getName()).log(Level.SEVERE, null, ex);
         }
 
