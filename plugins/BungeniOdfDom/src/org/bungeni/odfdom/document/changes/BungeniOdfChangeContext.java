840a54a836be3e348c53362aad9a184daa893ba9
ashok.hariharan <kohsah@users.noreply.github.com>
2010-03-15 15:26:42 +0000

    Adding change context class skeleton

---
+++ plugins/BungeniOdfDom/src/org/bungeni/odfdom/document/changes/BungeniOdfChangeContext.java
@@ -0,0 +1,19 @@
+package org.bungeni.odfdom.document.changes;
+
+/**
+ * This class provides the context for a particular change in a document.
+ * Context is provided in terms of depth of paragraph and no. of words from the start
+ * of the paragraph.
+ * @author Ashok Hariharan
+ */
+public class BungeniOdfChangeContext {
+
+    String formatString ;
+
+
+    public BungeniOdfChangeContext(){
+        //
+    }
+
+
+}
