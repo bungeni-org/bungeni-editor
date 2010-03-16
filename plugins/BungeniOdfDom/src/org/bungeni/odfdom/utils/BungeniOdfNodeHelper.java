813939770511027a965337f05b0527e1b96046b9
ashok.hariharan <kohsah@users.noreply.github.com>
2010-03-16 16:08:30 +0000

    Added Node XPAth calculator

--- plugins/BungeniOdfDom/src/org/bungeni/odfdom/utils/BungeniOdfNodeHelper.java
+++ plugins/BungeniOdfDom/src/org/bungeni/odfdom/utils/BungeniOdfNodeHelper.java
@@ -4,6 +4,8 @@ package org.bungeni.odfdom.utils;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
@@ -13,6 +15,88 @@ import org.w3c.dom.NodeList;
  * @author Ashok Hariharan
  */
 public class BungeniOdfNodeHelper {
+
+ /**
+  * Gets XPath of a Node object
+  * Adapted fromhttp://www.wadoku.de/wiki/display/DEV/XPath+of+Node
+  * By Mikkel Flindt Heisterberg
+  * 
+  * @param n
+  * @return
+  */
+    public static String getXPath(Node aNode) {
+      /** return if node is null **/
+      if (null == aNode) return null;
+
+      /** parent node **/
+      Node parent = null;
+      /** stack nodes in the parent hierarchy **/
+      Stack<Node> hierarchy = new Stack<Node>();
+      /** buffer to store node path **/
+      StringBuffer buffer = new StringBuffer();
+
+      /** push the current node into the node hierarchy **/
+      hierarchy.push(aNode);
+
+      parent = aNode.getParentNode();
+      /** go up the hierarchy upto the document node **/
+      while (null != parent && parent.getNodeType() != Node.DOCUMENT_NODE) {
+         /** push the parent on to the hierafchy stack **/
+         hierarchy.push(parent);
+         /** travel up the parent hierarchy **/
+         parent = parent.getParentNode();
+      }
+      /** build the XPath here **/
+      Object obj = null;
+      while (!hierarchy.isEmpty() && null != (obj = hierarchy.pop())) {
+         Node node = (Node) obj;
+         boolean handled = false;
+         /** we only process elements since we are primarily going to use it for text:change nodes **/
+         if (node.getNodeType() == Node.ELEMENT_NODE) {
+            Element e = (Element) node;
+            // is this the root element?
+            if (buffer.length() == 0) {
+               // root element - simply append element name
+               buffer.append(node.getNodeName());
+            } else {
+               // child element - append slash and element name
+               buffer.append("/");
+               buffer.append(node.getNodeName());
+
+               if (node.hasAttributes()) {
+                  // see if the element has a name or id attribute
+                  if (e.hasAttribute("id")) {
+                     // id attribute found - use that
+                     buffer.append("[@id='" + e.getAttribute("id") + "']");
+                     handled = true;
+                  } else if (e.hasAttribute("name")) {
+                     // name attribute found - use that
+                     buffer.append("[@name='" + e.getAttribute("name") + "']");
+                     handled = true;
+                  }
+               }
+
+               if (!handled) {
+                  // no known attribute we could use - get sibling index
+                  int prev_siblings = 1;
+                  Node prev_sibling = node.getPreviousSibling();
+                  while (null != prev_sibling) {
+                     if (prev_sibling.getNodeType() == node.getNodeType()) {
+                        if (prev_sibling.getNodeName().equalsIgnoreCase(node.getNodeName())) {
+                           prev_siblings++;
+                        }
+                     }
+                     prev_sibling = prev_sibling.getPreviousSibling();
+                  }
+                  buffer.append("[" + prev_siblings + "]");
+               }
+            }
+         }
+      }
+
+      // return buffer
+      return buffer.toString();
+   }
 
 
 
@@ -25,11 +109,16 @@ public class BungeniOdfNodeHelper {
         if (aNode == null) {
             return null;
         }
+        if (aNode.getNodeType() == Node.DOCUMENT_NODE) {
+            /** this is the parent node **/
+            return "/" + ((Document)aNode).getDocumentElement().getLocalName();
+        }
+
         if (aNode.getNodeType() == Node.ELEMENT_NODE) {
             Node aNodeParent = aNode.getParentNode();
             /** if the parent is null the aNode is at root **/
             if (aNodeParent == null) {
-                return "/" + aNode.getNodeName();
+                return "/" + aNode.getLocalName();
             }
             /** build a stack of parent nodes **/
             Stack<Node> stack = new Stack<Node>();
@@ -43,6 +132,7 @@ public class BungeniOdfNodeHelper {
             while (!stack.isEmpty()) {
                 pathStr.append(getNodeXPath(stack.pop()));
             }
+            System.out.println(pathStr.toString());
             return pathStr.toString();
         }
         if (aNode.getNodeType() == Node.ATTRIBUTE_NODE) {
@@ -53,7 +143,7 @@ public class BungeniOdfNodeHelper {
             String parentNodePath = getNodeXPath(aNodeParentOfAttr);
             StringBuffer attrXpathStr = new StringBuffer(parentNodePath)
                                         .append("[@")
-                                        .append(aNode.getNodeName())
+                                        .append(aNode.getLocalName())
                                         .append("='")
                                         .append(aNode.getTextContent())
                                         .append("']");
