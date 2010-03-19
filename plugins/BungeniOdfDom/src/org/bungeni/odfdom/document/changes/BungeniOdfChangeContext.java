f84a3536248869bc005fc51ba7364190f50eeac0
ashok.hariharan <kohsah@users.noreply.github.com>
2010-03-19 09:39:28 +0000

    API to get surrounding text - TODO - look for change-end , presently looks for text:change or text:change-start

--- plugins/BungeniOdfDom/src/org/bungeni/odfdom/document/changes/BungeniOdfChangeContext.java
+++ plugins/BungeniOdfDom/src/org/bungeni/odfdom/document/changes/BungeniOdfChangeContext.java
@@ -3,13 +3,15 @@ package org.bungeni.odfdom.document.changes;
 //~--- non-JDK imports --------------------------------------------------------
 
 
-import org.odftoolkit.odfdom.OdfElement;
+import java.util.ArrayList;
+import java.util.List;
 
 //~--- JDK imports ------------------------------------------------------------
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
 import org.apache.log4j.Logger;
+import org.bungeni.odfdom.document.BungeniOdfDocumentHelper;
 import org.bungeni.odfdom.utils.BungeniOdfNodeHelper;
 import org.bungeni.odfdom.utils.XPathComponent;
 import org.w3c.dom.Node;
@@ -21,14 +23,20 @@ import org.w3c.dom.Node;
  * @author Ashok Hariharan
  */
 public class BungeniOdfChangeContext {
+
+     private static org.apache.log4j.Logger log             =
+        Logger.getLogger(BungeniOdfChangeContext.class.getName());
+
+
+    BungeniOdfDocumentHelper m_docHelper;
     Node changeElement;
     String elementXpath;
     String     formatString;
-     private static org.apache.log4j.Logger log             =
-        Logger.getLogger(BungeniOdfChangeContext.class.getName());
-    public BungeniOdfChangeContext(Node changeElement) {
+
+    public BungeniOdfChangeContext(Node changeElement, BungeniOdfDocumentHelper odfHelper) {
         this.changeElement = changeElement;
         this.elementXpath = BungeniOdfNodeHelper.getXPath(changeElement);
+        this.m_docHelper = odfHelper;
     }
 
     public String getReportPath() {
@@ -37,6 +45,27 @@ public class BungeniOdfChangeContext {
         // office:document-content/office:body[1]/office:text[1]/text:section[1]/text:section[5]/text:section[1]/text:section[2]/text:p[4]/text:change-start[3]
         String[] xpathComponents = xPathString.split("/");
         StringBuffer sReportMessage = new StringBuffer();
+        List<String> surrounds = getSurroundingText();
+        for (int i = 0; i < surrounds.size(); i++) {
+            String string = surrounds.get(i);
+            if (i == 0 ) {
+                if (string.trim().length() > 0) {
+                    sReportMessage.append(" After :\n");
+                    sReportMessage.append(string + "\n");
+                }
+            } else {
+                if (string.trim().length() > 0) {
+                    sReportMessage.append(" Before :\n");
+                    sReportMessage.append(string + "\n");
+                }
+            }
+        }
+        for (String string : surrounds) {
+            if (string.trim().length() > 0 ) {
+                sReportMessage.append(string);
+            }
+            
+        }
         for (int i = xpathComponents.length - 2; i >= 0; i--) {
             String xpathComponent = xpathComponents[i];
             sReportMessage.append(parsePathComponent(xpathComponent));
@@ -45,6 +74,21 @@ public class BungeniOdfChangeContext {
         return sReportMessage.toString();
     }
 
+    private List<String> getSurroundingText(){
+        ArrayList<String> listSurrounding = new ArrayList<String>(0);
+        try {
+            String precedingXpath = this.elementXpath + "/preceding-sibling::text()";
+            String followingXpath = this.elementXpath + "/following-sibling::text()";
+            XPath xPath = m_docHelper.getOdfDocument().getXPath();
+            String precedingText = (String) xPath.evaluate(precedingXpath, m_docHelper.getOdfDocument().getContentDom(), XPathConstants.STRING);
+            String followingText = (String) xPath.evaluate(followingXpath, m_docHelper.getOdfDocument().getContentDom(), XPathConstants.STRING);
+            listSurrounding.add(precedingText);
+            listSurrounding.add(followingText);
+        } catch (Exception ex) {
+            log.error(ex.getMessage());
+        }
+        return listSurrounding;
+    }
 
     private String parsePathComponent(String xpathComponent) {
         XPathComponent objComponent = new XPathComponent(xpathComponent);
@@ -70,11 +114,11 @@ public class BungeniOdfChangeContext {
     }
 
     private String pathMessage_p(XPathComponent xpathComponent) {
-        return " in Paragraph " + xpathComponent.getIndex();
+        return " In Paragraph " + xpathComponent.getIndex() + " ;";
     }
 
     private String pathMessage_section(XPathComponent xpathComponent) {
-        return " in Section " + xpathComponent.getIndex();
+        return " in Section " + xpathComponent.getIndex() + " ;";
     }
 
     private String pathMessage_change_start(XPathComponent xpathComponent) {
