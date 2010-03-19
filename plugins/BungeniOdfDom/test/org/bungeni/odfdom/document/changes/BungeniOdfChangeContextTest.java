aa57da8c164e62f6a8a79a61ffe3162e6f8b74e0
ashok.hariharan <kohsah@users.noreply.github.com>
2010-03-19 09:37:44 +0000

    unit test for change context

---
+++ plugins/BungeniOdfDom/test/org/bungeni/odfdom/document/changes/BungeniOdfChangeContextTest.java
@@ -0,0 +1,68 @@
+package org.bungeni.odfdom.document.changes;
+
+import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import org.bungeni.odfdom.document.BungeniOdfDocumentHelper;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.w3c.dom.Node;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author undesa
+ */
+public class BungeniOdfChangeContextTest {
+    BungeniOdfDocumentHelper dochelper = null;
+
+    public BungeniOdfChangeContextTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+try {
+            dochelper = new BungeniOdfDocumentHelper(new File("testdocs/test-xpath-calculate.odt"));
+        } catch (Exception ex) {
+            Logger.getLogger(BungeniOdfChangeContextTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getReportPath method, of class BungeniOdfChangeContext.
+     */
+    @Test
+    public void testGetReportPath() {
+        try {
+            System.out.println("getReportPath");
+            XPath xPath = dochelper.getOdfDocument().getXPath();
+            String expResult = "office:document-content/office:body[1]/office:text[1]/text:section[1]/text:section[5]/text:section[1]/text:section[2]/text:p[4]/text:change-start[3]";
+            Node foundNode = (Node) xPath.evaluate("//text:change-start[@text:change-id='ct-1413428116']", dochelper.getOdfDocument().getContentDom(), XPathConstants.NODE);
+            BungeniOdfChangeContext cxt = new BungeniOdfChangeContext(foundNode, dochelper);
+            String reportPath = cxt.getReportPath();
+            System.out.println(reportPath);
+            assertEquals(true, true);
+        } catch (Exception ex) {
+            Logger.getLogger(BungeniOdfChangeContextTest.class.getName()).log(Level.SEVERE, null, ex);
+        } 
+    }
+
+}
