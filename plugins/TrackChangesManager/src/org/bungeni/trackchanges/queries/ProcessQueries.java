44104741b5895de9a832816bcc6e814ad3a2cc16
ashok.hariharan <kohsah@users.noreply.github.com>
2010-04-18 21:41:15 +0000

    Static queries for processing

--- plugins/TrackChangesManager/src/org/bungeni/trackchanges/queries/ProcessQueries.java
+++ plugins/TrackChangesManager/src/org/bungeni/trackchanges/queries/ProcessQueries.java
@@ -7,12 +7,41 @@ package org.bungeni.trackchanges.queries;
 public class ProcessQueries {
 
     public static String INSERT_PROCESS (String processId, String billId) {
-        return "insert into process ( process_id, bill_id) " +
+        return "insert into process ( process_id, bill_id, process_date) " +
                 "values ('"+
                             processId
                             + "','" +
                             billId
-                 + "')";
+                            + "'," +
+                            "now() "
+                 + ")";
+    }
+
+    public static String GET_LATEST_PROCESS_FOR_BILL(String billId) {
+        String strQuery = "select process_id, bill_id, process_date from process " +
+                "where bill_id = '"+ billId + "' and process_date in (" +
+                "   select max(process_date) from process " +
+                ")";
+        return strQuery ;
+    }
+    
+    public static String GET_LATEST_DOCS_FOR_BILL(String billId) {
+        String strQuery = "" +
+                "select process_id, doc_name, doc_path, doc_owner   from process_documents where " +
+                "process_id in ( " +
+                    "select process_id from process " +
+                    "where process_date in (  " +
+                        "select max(process_date) from process  " +
+                        "where bill_id = '" + billId + "'" +
+                     ")" +
+                ")";
+        return strQuery;
+    }
+
+    public static String GET_LATEST_AMENDS(String processId, String docName, String docAuthor) {
+        String strQuery = " select process_id, doc_name, doc_owner, change_id, change_action, change_date, change_text, change_status " +
+                " from process_amends where process_id = '" + processId + "' and doc_name='" + docName + "' doc_author = '" + docAuthor + "' ";
+        return strQuery;
     }
 
 
@@ -27,6 +56,36 @@ public class ProcessQueries {
                  + "','" +
                  docAuthor
                  + "')";
+    }
+
+    public static String INSERT_PROCESS_AMENDS (
+            String processId,
+            String docName,
+            String docAuthor,
+            String changeId,
+            String changeType ,
+            String changeDate,
+            String changeText,
+            String changeStatus ) {
+        return "insert into process_amends " +
+                "(PROCESS_ID, DOC_NAME, DOC_OWNER, CHANGE_ID, CHANGE_ACTION, CHANGE_DATE, CHANGE_TEXT, CHANGE_STATUS)" +
+                " values ('" +
+                processId
+                + "','"+
+                docName
+                + "','"+
+                docAuthor
+                + "','" +
+                changeId
+                + "','" +
+                changeType
+                + "','" +
+                changeDate
+                + "','" +
+                changeText
+                + "'," +
+                changeStatus
+                + ")";
     }
 
 }
