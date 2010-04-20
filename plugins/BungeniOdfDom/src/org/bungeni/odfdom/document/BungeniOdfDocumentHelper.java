c9894a810f09b054fa893786a6cf64f7ab779e4d
ashok.hariharan <kohsah@users.noreply.github.com>
2010-04-20 08:54:55 +0000

    Added crc check API to compare if file has changed

--- plugins/BungeniOdfDom/src/org/bungeni/odfdom/document/BungeniOdfDocumentHelper.java
+++ plugins/BungeniOdfDom/src/org/bungeni/odfdom/document/BungeniOdfDocumentHelper.java
@@ -2,14 +2,12 @@ package org.bungeni.odfdom.document;
 
 //~--- non-JDK imports --------------------------------------------------------
 
-import java.io.File;
-import java.net.URI;
-import java.net.URISyntaxException;
 import org.apache.log4j.Logger;
 
 import org.bungeni.odfdom.document.changes.BungeniOdfTrackedChangesHelper;
 import org.bungeni.odfdom.document.properties.BungeniOdfPropertiesHelper;
 import org.bungeni.odfdom.section.BungeniOdfSectionHelper;
+
 import org.odftoolkit.odfdom.doc.OdfDocument;
 import org.odftoolkit.odfdom.doc.office.OdfOfficeMasterStyles;
 import org.odftoolkit.odfdom.doc.style.OdfStyleBackgroundImage;
@@ -22,9 +20,21 @@ import org.w3c.dom.NodeList;
 
 //~--- JDK imports ------------------------------------------------------------
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import java.util.logging.Level;
+import java.util.zip.CRC32;
+import java.util.zip.CheckedInputStream;
 
 /**
- *<p>
+ * <p>
  * This class provides a wrapper on the ODFDOM odf library. ODFDOM primarily provides access
  * to the DOM of an ODF file -- BungeniOdfDOM provides a helper API on that to access common
  * document elements like properties, sections and track changes
@@ -32,11 +42,22 @@ import org.w3c.dom.NodeList;
  * @author Ashok Hariharan
  */
 public class BungeniOdfDocumentHelper {
-    private static org.apache.log4j.Logger log = Logger.getLogger(BungeniOdfDocumentHelper.class.getName());
+    private static org.apache.log4j.Logger log                =
+        Logger.getLogger(BungeniOdfDocumentHelper.class.getName());
+    private BungeniOdfTrackedChangesHelper s_changesHelper    = null;
+    private BungeniOdfPropertiesHelper     s_propertiesHelper = null;
+    private BungeniOdfSectionHelper        s_sectionHelper    = null;
     private OdfDocument                    odfDocument;
-    private BungeniOdfSectionHelper s_sectionHelper = null;
-    private BungeniOdfPropertiesHelper s_propertiesHelper = null;
-    private BungeniOdfTrackedChangesHelper s_changesHelper = null;
+
+    /**
+     * <p>This API initializes the object using a File handle to a ODF document.
+     * The OdfDocument handle is created internally by the object</p>
+     * @param fodfFile
+     * @throws java.lang.Exception
+     */
+    public BungeniOdfDocumentHelper(File fodfFile) throws Exception {
+        this.odfDocument = OdfDocument.loadDocument(fodfFile);
+    }
 
     /**
      * <p>Initialize the object using the ODFDOM OdfDocument handle
@@ -45,16 +66,6 @@ public class BungeniOdfDocumentHelper {
      */
     public BungeniOdfDocumentHelper(OdfDocument doc) {
         this.odfDocument = doc;
-    }
-
-    /**
-     *<p>This API initializes the object using a File handle to a ODF document.
-     * The OdfDocument handle is created internally by the object</p>
-     * @param fodfFile
-     * @throws java.lang.Exception
-     */
-    public BungeniOdfDocumentHelper(File fodfFile) throws Exception {
-        this.odfDocument = OdfDocument.loadDocument(fodfFile);
     }
 
     /**
@@ -70,51 +81,55 @@ public class BungeniOdfDocumentHelper {
      * @return String containing path to the document
      */
     public String getDocumentPath() {
-        String sPath= "";
-        try {
-            URI fURI = new URI(odfDocument.getBaseURI());
-            File fDoc = new File(fURI);
-            sPath = fDoc.getPath();
+        String sPath = "";
 
+        try {
+            URI  fURI = new URI(odfDocument.getBaseURI());
+            File fDoc = new File(fURI);
+
+            sPath = fDoc.getPath();
         } catch (URISyntaxException ex) {
             log.error("getDocumentPath :" + ex.getMessage(), ex);
         }
-        return sPath;
 
+        return sPath;
     }
+
     /**
      * <p>Returns a BungeniOdfSectionHelper object (non static singleton)</p>
      * @return
      */
     public BungeniOdfSectionHelper getSectionHelper() {
-        if (s_sectionHelper == null ) {
+        if (s_sectionHelper == null) {
             s_sectionHelper = new BungeniOdfSectionHelper(this);
         }
+
         return s_sectionHelper;
     }
 
-     /**
+    /**
      * <p>Returns a BungeniOdfPropertiesHelper object</p>
      * @return
      */
     public BungeniOdfPropertiesHelper getPropertiesHelper() {
-        if (s_propertiesHelper == null ) {
-           s_propertiesHelper =  new BungeniOdfPropertiesHelper(this);
+        if (s_propertiesHelper == null) {
+            s_propertiesHelper = new BungeniOdfPropertiesHelper(this);
         }
+
         return s_propertiesHelper;
     }
 
-     /**
+    /**
      * <p>Returns a BungeniOdfTrackedChangesHelper object</p>
      * @return
      */
     public BungeniOdfTrackedChangesHelper getChangesHelper() {
-        if (s_changesHelper == null ) {
-           s_changesHelper =  new BungeniOdfTrackedChangesHelper(this);
+        if (s_changesHelper == null) {
+            s_changesHelper = new BungeniOdfTrackedChangesHelper(this);
         }
+
         return s_changesHelper;
     }
-
 
     /**
      * <p>Get the standard page layout for the document</p>
@@ -125,8 +140,9 @@ public class BungeniOdfDocumentHelper {
 
         try {
             OdfOfficeMasterStyles mastersStyles  = this.odfDocument.getOfficeMasterStyles();
-            OdfStyleMasterPage   standardPage   = mastersStyles.getMasterPage("Standard");
-            String          pageLayoutName = standardPage.getStylePageLayoutNameAttribute();
+            OdfStyleMasterPage    standardPage   = mastersStyles.getMasterPage("Standard");
+            String                pageLayoutName = standardPage.getStylePageLayoutNameAttribute();
+
             standardLayout = odfDocument.getStylesDom().getAutomaticStyles().getPageLayout(pageLayoutName);
         } catch (Exception ex) {
             log.error("getStandardPageLayout : ", ex);
@@ -137,7 +153,7 @@ public class BungeniOdfDocumentHelper {
 
     /**
      * <p>Removes the background image for the page if it has one</p>
-     * 
+     *
      */
     public void removeBackgroundImage() {
         try {
@@ -147,7 +163,7 @@ public class BungeniOdfDocumentHelper {
 
             for (int i = 0; i < bgImageNodes.getLength(); i++) {
                 OdfStyleBackgroundImage bgImage    = (OdfStyleBackgroundImage) bgImageNodes.item(i);
-                Node               parentNode = bgImage.getParentNode();
+                Node                    parentNode = bgImage.getParentNode();
 
                 if (parentNode != null) {
                     if (parentNode.getNodeName().equals("style:page-layout-properties")) {
@@ -161,10 +177,33 @@ public class BungeniOdfDocumentHelper {
     }
 
     /**
+     * Returns the checksum of the odf document
+     * @return
+     */
+    public long getChecksum() {
+        long crc = 0;
+        try {
+            FileInputStream     fs    = new FileInputStream(getDocumentPath());
+            CheckedInputStream  check = new CheckedInputStream(fs, new CRC32());
+            BufferedInputStream in    = new BufferedInputStream(check);
+            while (in.read() != -1) {
+                // Read file in completely
+            }
+            crc = check.getChecksum().getValue();
+            in.close();
+            check.close();
+            fs.close();
+        } catch (IOException ex) {
+            log.error("getChecksum :" + ex.getMessage());
+        }
+        return crc;
+    }
+
+    /**
      * Saves the current document to its current path
      * @return
      */
-    public boolean saveDocument(){
+    public boolean saveDocument() {
         return saveDocument(this.getDocumentPath());
     }
 
@@ -174,13 +213,16 @@ public class BungeniOdfDocumentHelper {
      */
     public boolean saveDocument(String fpath) {
         boolean bState = false;
+
         try {
             OdfPackage docPackage = this.odfDocument.getPackage();
+
             this.odfDocument.save(fpath);
             bState = true;
         } catch (Exception ex) {
-            log.error("saveDocument : "  + ex);
+            log.error("saveDocument : " + ex);
         }
+
         return bState;
     }
 
@@ -191,13 +233,16 @@ public class BungeniOdfDocumentHelper {
      */
     public boolean saveDocument(File foutputfile) {
         boolean bState = false;
+
         try {
             OdfPackage docPackage = this.odfDocument.getPackage();
+
             this.odfDocument.save(foutputfile);
             bState = true;
         } catch (Exception ex) {
-            log.error("saveDocument : "  + ex);
+            log.error("saveDocument : " + ex);
         }
+
         return bState;
     }
 
@@ -205,11 +250,12 @@ public class BungeniOdfDocumentHelper {
      * It is neccessary to close a document if you want to process an odf document across different threads.
      * e.g. acrosss swingworker threads... you get an odfdocumenthelper from the first thread save the document,
      * close the handle ... and do the same from the other
-     * 
+     *
      */
-    public void closeDocument(){
-            OdfPackage docPackage = this.odfDocument.getPackage();
-            this.odfDocument.close();
-            docPackage.close();
+    public void closeDocument() {
+        OdfPackage docPackage = this.odfDocument.getPackage();
+
+        this.odfDocument.close();
+        docPackage.close();
     }
 }
