73690069272840bb735d3e7d06cddd0d0c8db992
ashok.hariharan <kohsah@users.noreply.github.com>
2010-04-20 15:36:43 +0000

    Adding merge package and class placeholder

---
+++ plugins/BungeniOdfDom/src/org/bungeni/odfdom/document/merge/BungeniOdfMerge.java
@@ -0,0 +1,13 @@
+package org.bungeni.odfdom.document.merge;
+
+/**
+ *
+ * @author Ashok Hariharan
+ */
+public class BungeniOdfMerge {
+    /***
+     *
+     * Placeholder for merge code
+     *
+     */
+}
