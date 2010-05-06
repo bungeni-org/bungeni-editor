dae0f9782f0f9d48654ad6290ee13290e9ab90a2
ashok.hariharan <kohsah@users.noreply.github.com>
2010-05-06 22:28:26 +0000

    Extract track changes container , and sequence-decls container

--- plugins/xmlMultiMerge/src/org/bungeni/xmlmerge/xmlMergeMain.java
+++ plugins/xmlMultiMerge/src/org/bungeni/xmlmerge/xmlMergeMain.java
@@ -1,16 +1,10 @@
 package org.bungeni.xmlmerge;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpressionException;
@@ -18,12 +12,19 @@ import javax.xml.xpath.XPathFactory;
 import org.apache.log4j.BasicConfigurator;
 import org.bungeni.db.BungeniClientDB;
 import org.bungeni.db.QueryResults;
+import org.bungeni.odfdom.document.BungeniOdfDocumentHelper;
+import org.bungeni.odfdom.utils.BungeniOdfNodeHelper;
 import org.bungeni.xmlmerge.queries.xmlMergeQueries;
 import org.bungeni.xmlmerge.utils.XPathUtils;
+import org.odftoolkit.odfdom.doc.OdfDocument;
+import org.odftoolkit.odfdom.doc.text.OdfTextChange;
+import org.odftoolkit.odfdom.doc.text.OdfTextChangeStart;
+import org.odftoolkit.odfdom.doc.text.OdfTextSequenceDecls;
+import org.odftoolkit.odfdom.doc.text.OdfTextTrackedChanges;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
 /**
  *
@@ -31,19 +32,15 @@ import org.xml.sax.SAXException;
  */
 public class xmlMergeMain {
 
-    private Document originalFile ;
-    private List<Document> fileVersions = new ArrayList<Document>(0);
+    private OdfDocument originalFile ;
+    private List<OdfDocument> fileVersions = new ArrayList<OdfDocument>(0);
       private static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(xmlMergeMain.class.getName());
       private BungeniClientDB mergeDB = null;
 
 
-    public xmlMergeMain(Document origFile, List<Document> versions){
+    public xmlMergeMain(OdfDocument origFile, List<OdfDocument> versions){
         this.originalFile = origFile;
         this.fileVersions = versions;
-        log.debug(originalFile.getDocumentURI());
-        for (Document document : versions) {
-            log.debug(document.getDocumentURI());
-        }
         this.mergeDB = getDBhandle();
     }
 
@@ -55,8 +52,7 @@ public class xmlMergeMain {
 
     public void buildChangeInfo()  {
         //create an xpath object
-        XPathFactory xpathFactory = XPathFactory.newInstance();
-        XPath xPath = xpathFactory.newXPath();
+        XPath xPath = originalFile.getXPath();
         //build the change node points in the db in the document order
         buildChangeNodePoints(xPath);
         processChangesInOrder(xPath);
@@ -147,23 +143,51 @@ public class xmlMergeMain {
            return cpos;
     }
 
-    private void buildChangeNodePoints(XPath xPath) {
+
+    private void extractTrackChangesContainer(OdfDocument document) {
+        BungeniOdfDocumentHelper odoc = new BungeniOdfDocumentHelper(document);
+        OdfTextTrackedChanges tcElement = (OdfTextTrackedChanges) odoc.getChangesHelper().getTrackedChangeContainer();
+        //write to disk
+    }
+
+    private void extractTextSequenceDecls (OdfDocument document) throws XPathExpressionException, Exception {
+        XPath xPath = document.getXPath();
+        OdfTextSequenceDecls seqDecls = (OdfTextSequenceDecls) xPath.evaluate("//text:sequence-decls", document.getContentDom(), XPathConstants.NODE);
+        //write to disk
+    }
+
+    
+    private void buildChangeNodePoints(XPath xPath) throws Exception {
         List<String> addQueries = new ArrayList<String>(0);
         NodeList nodeListPrev = null;
-        for (Document document : fileVersions) {
+        for (OdfDocument document : fileVersions) {
             try {
-                String xpathExpr = "//change-start";
-                NodeList changeStarts = (NodeList) xPath.evaluate(xpathExpr, document, XPathConstants.NODESET);
+                //extract track changes container
+                extractTrackChangesContainer(document);
+                //extract sequence decls
+                extractTextSequenceDecls(document);
+                //find all change start nodes
+                String xpathExpr = "//node()[name()='text:change-start' or name()='text:change']";
+                NodeList changeNodes = (NodeList) xPath.evaluate(xpathExpr, document.getContentDom(), XPathConstants.NODESET);
+                //iterate through the change nodes
+                for (int i = 0; i < changeNodes.getLength(); i++) {
+                    String xpathStart = "", xpathEnd = "";
+                    Node changeStart = changeNodes.item(i);
+                    Node changeEnd = null;
+                    String changeId = changeStart.getAttributes().getNamedItem("text:change-id").getTextContent();
+                    xpathStart = XPathUtils.getXPath(changeStart);
+                    //check if the node is a change start node
+                    if (changeStart.getNodeName().equals(OdfTextChangeStart.ELEMENT_NAME.getQName())){
+                        //get the change id
+                        //get the node path to the change-start item
+                        //get the xpath to the expresion end
+                        String xpathExprEnd = "//text:change-end[@text:change-id='" + changeId + "']";
+                        changeEnd = (Node) xPath.evaluate(xpathExprEnd, document.getContentDom(), XPathConstants.NODE);
+                        xpathEnd = BungeniOdfNodeHelper.getXPath(changeEnd);
+                    }
 
-                for (int i = 0; i < changeStarts.getLength(); i++) {
-                    Node changeStart = changeStarts.item(i);
-                    String changeId = changeStart.getAttributes().getNamedItem("change-id").getTextContent();
-                    String xpathExprEnd = "//change-end[@change-id='" + changeId + "']";
-                    Node changeEnd = (Node) xPath.evaluate(xpathExprEnd, document, XPathConstants.NODE);
-                    String strXpathStart = XPathUtils.getXPath(changeStart);
-                    String strXpathEnd = XPathUtils.getXPath(changeEnd);
-                    String addQuery = xmlMergeQueries.ADD_CHANGE(document.getDocumentURI(), changeId, "", strXpathStart,
-                            strXpathEnd, "", Integer.toString(i));
+                    String addQuery = xmlMergeQueries.ADD_CHANGE(document.getBaseURI(), changeId, "", xpathStart,
+                            xpathEnd, "", Integer.toString(i));
                     addQueries.add(addQuery);
                     log.debug("buildChangeInfo : " + addQuery);
                 }
@@ -190,36 +214,29 @@ public class xmlMergeMain {
         try {
             BasicConfigurator.configure();
             final String userDir = System.getProperty("user.dir");
-            final String originalFile = userDir + "/testfiles/original.xml";
+            final String originalFile = userDir + "/testfiles/original.odt";
             List<String> versions = new ArrayList<String>() {
 
                 {
-                    add(userDir + "/testfiles/version-1.xml");
-                    add(userDir + "/testfiles/version-2.xml");
-                    add(userDir + "/testfiles/version-3.xml");
+                    add(userDir + "/testfiles/version-1.odt");
+                    add(userDir + "/testfiles/version-2.odt");
                 }
             };
-            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder builder = factory.newDocumentBuilder();
 
-            Document xmlOrigFile = builder.parse(new File(originalFile));
-            List<Document> xmlVersions = new ArrayList<Document>(0);
+            OdfDocument xmlOrigFile = OdfDocument.loadDocument(new File(originalFile));
+            List<OdfDocument> xmlVersions = new ArrayList<OdfDocument>(0);
 
             for (String version : versions) {
-               xmlVersions.add(builder.parse(new File(version)));
+               xmlVersions.add(OdfDocument.loadDocument(new File(version)));
             }
 
             xmlMergeMain mergeMain = new xmlMergeMain (xmlOrigFile, xmlVersions);
             mergeMain.cleanupChangeInfo();
             mergeMain.buildChangeInfo();
 
-        } catch (SAXException ex) {
+        } catch (java.lang.Exception ex) {
             log.error(ex.getMessage());
-        } catch (IOException ex) {
-            log.error(ex.getMessage());
-        } catch (ParserConfigurationException ex) {
-            log.error(ex.getMessage());
-        }
+        } 
 
 
 
