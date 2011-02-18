19863a9049c57693224f6e3ff222cc36cafadf58
ashok.hariharan <kohsah@users.noreply.github.com>
2011-02-18 14:24:19 +0000

    Unit test placeholder

---
+++ OpenOfficeHelper/branches/rdf-migrate/OOoBungeniLib/test/org/bungeni/ooo/OOoTestCase.java
@@ -0,0 +1,68 @@
+
+package org.bungeni.ooo;
+
+
+import com.sun.star.comp.helper.Bootstrap;
+import com.sun.star.frame.XComponentLoader;
+import com.sun.star.frame.XDesktop;
+import com.sun.star.lang.XComponent;
+import com.sun.star.lang.XMultiComponentFactory;
+import com.sun.star.uno.XComponentContext;
+import java.io.File;
+import javax.swing.JFileChooser;
+import junit.framework.TestCase;
+import org.junit.BeforeClass;
+
+/**
+ *
+ * @author undesa
+ */
+public class OOoTestCase extends TestCase {
+    public XComponentContext m_xContext;
+    public XMultiComponentFactory m_ooMCF;
+    public XDesktop m_ooDesktop;
+    public OOComponentHelper ooDoc;
+    public XComponentLoader m_ooComponentLoader;
+
+    public OOoTestCase(String name) {
+        super(name);
+        initoOo();
+    }
+
+    @BeforeClass
+    public void testInit(String pathToFile) {
+        File fileName = new File(pathToFile);
+        XComponent xComp = OOComponentHelper.openExistingDocument(fileName.getAbsolutePath(), m_ooComponentLoader);
+        if (xComp == null) {
+            fail("XComponent was null for : "+ fileName.getAbsolutePath());
+        }
+        ooDoc = new OOComponentHelper(xComp, m_xContext);
+        assertNotNull("OOComponentHandle was not initialized", ooDoc);
+
+    }
+
+    private void initoOo() {
+        try {
+            m_xContext = Bootstrap.bootstrap();
+            if (m_xContext == null) {
+                fail("OpenOffice wasnt initialized");
+            }
+            if (m_ooMCF == null) {
+                m_ooMCF = m_xContext.getServiceManager();
+            }
+            if (m_ooMCF != null) {
+                Object oDesktop = m_ooMCF.createInstanceWithContext("com.sun.star.frame.Desktop", m_xContext);
+                // (4a) get the XDesktop interface object
+                m_ooDesktop = (com.sun.star.frame.XDesktop) com.sun.star.uno.UnoRuntime.queryInterface(
+                        com.sun.star.frame.XDesktop.class, oDesktop);
+
+                // (4b) get the desktop's component loader interface object
+                m_ooComponentLoader = (com.sun.star.frame.XComponentLoader) com.sun.star.uno.UnoRuntime.queryInterface(
+                        com.sun.star.frame.XComponentLoader.class, m_ooDesktop);
+            }
+        } catch (java.lang.Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
