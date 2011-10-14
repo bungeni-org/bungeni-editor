4176ba68368a3fb89a3b1d7a1651e1252293a924
aowino <aowino@gmail.com>
2011-10-14 11:18:36 +0000

    Base class for bungeni xml test cases

---
+++ apps/BungeniTransformer/branches/generic-release/test/translatortest/OATranslatorTestBungeniXmlBase.java
@@ -0,0 +1,24 @@
+package translatortest;
+
+
+/**
+ *This is the base class used for creating unit tests for debaterecord type documents.
+ * The base class sets up the the translator configuration and the piple for the translation.
+ * Any deriving unit test class need only set :
+ *
+ * 	m_inputDocument = "test/testdocs/test_debaterecord.odt";
+	m_outputDocument = "test/testresults/test_debaterecord.xml";
+	m_outputMetalex = "test/testresults/test_debaterecord.mlx";
+	m_comparisonDocument = "test/testdocs/test_debaterecord_out.xml";
+ *
+ * @author Ashok Hariharan
+ */
+public class OATranslatorTestBungeniXmlBase extends OATranslatorTestBase {
+
+	public OATranslatorTestBungeniXmlBase() {
+		super();
+		setConfigFilePath("configfiles/configs/config_bungeniportal.xml");
+		//setPipeline("metalex2akn/minixslt/debaterecord/pipeline.xsl");
+	}
+}
+
