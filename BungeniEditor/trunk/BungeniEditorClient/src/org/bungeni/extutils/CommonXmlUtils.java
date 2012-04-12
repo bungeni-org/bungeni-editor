1923f96be2281f13082c2a9a93e249fa169176fe
ashok.hariharan <kohsah@users.noreply.github.com>
2012-04-12 10:35:03 +0000

    Do a convert to UTF8 only after checking if the string is NOT in UTF8 - otherwise encoding a UTF8 string into UTF8 twices causes weird errors See :http://groups.google.com/group/bungeni-editor/browse_thread/thread/54d9f495daa0387e for issue report

--- BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/extutils/CommonXmlUtils.java
+++ BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/extutils/CommonXmlUtils.java
@@ -84,12 +84,60 @@ public class CommonXmlUtils {
     private static String utf8String(String text){
         String returnText = "";
         try {
-            returnText = new String(text.getBytes(), "UTF8");
+            byte[] bytes = text.getBytes();
+            //!+UTF8_CHECK (ah, 12-04-2012) Do a convert to UTF8 only after
+            //checking if the string is NOT in UTF8 - otherwise encoding a UTF8
+            // string into UTF8 twices causes weird errors
+            // Issue reported by samar ayash.
+            if (validUTF8(bytes)){
+               returnText = text;
+            } else {
+                returnText = new String(text.getBytes(), "UTF8");
+            }
         } catch (UnsupportedEncodingException ex) {
             returnText = text;
         }
         return returnText;
     }
+
+  private static boolean validUTF8(byte[] input) {
+      int i = 0;
+      // Check for BOM
+      if (input.length >= 3 && (input[0] & 0xFF) == 0xEF
+        && (input[1] & 0xFF) == 0xBB & (input[2] & 0xFF) == 0xBF) {
+       i = 3;
+      }
+
+      int end;
+      for (int j = input.length; i < j; ++i) {
+       int octet = input[i];
+       if ((octet & 0x80) == 0) {
+        continue; // ASCII
+       }
+
+       // Check for UTF-8 leading byte
+       if ((octet & 0xE0) == 0xC0) {
+        end = i + 1;
+       } else if ((octet & 0xF0) == 0xE0) {
+        end = i + 2;
+       } else if ((octet & 0xF8) == 0xF0) {
+        end = i + 3;
+       } else {
+        // Java only supports BMP so 3 is max
+        return false;
+       }
+
+       while (i < end) {
+        i++;
+        octet = input[i];
+        if ((octet & 0xC0) != 0x80) {
+         // Not a valid trailing byte
+         return false;
+        }
+       }
+      }
+      return true;
+ }
 
     public static String getLocalizedChildElementValue(Element anElement, String localizedChild) {
         List<Element> childTitles = anElement.getChildren(localizedChild);
