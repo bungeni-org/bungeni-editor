241722db47c1e160e634a62becc38a35a9f43081
ashok.hariharan <kohsah@users.noreply.github.com>
2012-04-23 07:48:44 +0000

    Fixing unit tests for bungeni agena and quesiton conversion

---
+++ apps/BungeniTransformer/trunk/test/translatortest/TestBungeniXmlAgendaItem.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package translatortest;
+
+/**
+ *
+ * @author anthony
+ */
+public class TestBungeniXmlAgendaItem extends OATranslatorTestBungeniXmlBase {
+	public TestBungeniXmlAgendaItem(){
+		super();
+                //TransformerFactoryImpl l = new TransformerFactoryImpl();
+                setConfigFilePath("configfiles/configs/config_bungeni_parliamentaryitem.xml");
+		setInputDocument("test/testdocs/bungeni_agendaitem.xml");
+		setOutputDocument("test/testresults/test_bungeni_agendaitem.xml");
+		setOutputMetalex("test/testresults/test_bungeni_agendaitem.mlx");
+		setComparisonDocument("test/testdocs/test_bungeni_question_out.xml");
+	}
+
+}
