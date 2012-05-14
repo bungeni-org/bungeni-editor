99b01c96084891071c982932658277d20ccd39f9
ashok.hariharan <kohsah@users.noreply.github.com>
2012-05-14 06:44:38 +0000

    Updating unit tests for transformer

--- apps/BungeniTransformer/trunk/test/translatortest/TestAn1toAn2.java
+++ apps/BungeniTransformer/trunk/test/translatortest/TestAn1toAn2.java
@@ -10,7 +10,7 @@ package translatortest;
 public class TestAn1toAn2 extends OATranslatorTestBungeniXmlBase {
 	public TestAn1toAn2(){
 		super();
-                setConfigFilePath("configfiles/configs/config_an1_to_an2.xml");
+                setConfigFilePath("configfiles/configs/config_an1const_an2const.xml");
 		setInputDocument("test/testdocs/an1_act01.xml");
 		setOutputDocument("test/testresults/out_an1_act01.xml");
 		setOutputMetalex("test/testresults/out_an1_act01.mlx");
--- apps/BungeniTransformer/trunk/test/translatortest/TestBungeniXmlAgendaItem.java
+++ apps/BungeniTransformer/trunk/test/translatortest/TestBungeniXmlAgendaItem.java
@@ -17,7 +17,7 @@ public class TestBungeniXmlAgendaItem extends OATranslatorTestBungeniXmlBase {
 		setInputDocument("test/testdocs/bungeni_agendaitem.xml");
 		setOutputDocument("test/testresults/test_bungeni_agendaitem.xml");
 		setOutputMetalex("test/testresults/test_bungeni_agendaitem.mlx");
-		setComparisonDocument("test/testresults/test_bungeni_agendaitem_out.xml");
+		setComparisonDocument("test/testdocs/test_bungeni_agendaitem_out.xml");
 	}
 
 }
