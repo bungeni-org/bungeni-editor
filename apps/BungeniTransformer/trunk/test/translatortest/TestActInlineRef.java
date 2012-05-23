20ec178353733fc74de10fe0cae09a2210d868f4
smr.ayesh <smr.ayesh@gmail.com>
2012-05-23 12:18:58 +0000

    act

---
+++ apps/BungeniTransformer/trunk/test/translatortest/TestActInlineRef.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package translatortest;
+
+/**
+ *
+ * @author bzuadmin
+ */
+public class TestActInlineRef extends OATranslatorTestDebateBase {
+	public TestActInlineRef(){
+		super();
+		setInputDocument("test/testdocs/test_act_inline_ref.odt");
+		setOutputDocument("test/testresults/test_act_inline_ref.xml");
+		//setOutputMetalex("test/testresults/test_act_inline_ref_mlx.xml");
+		setComparisonDocument("test/testdocs/act_test_comp.xml");
+	}
+    
+}
