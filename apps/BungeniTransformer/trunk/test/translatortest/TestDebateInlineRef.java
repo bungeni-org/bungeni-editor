a1735889882f2d69195b901bcadef658b0c66d08
ashok.hariharan <kohsah@users.noreply.github.com>
2012-05-23 11:15:21 +0000

    Added unit test for inline ref transformation example

---
+++ apps/BungeniTransformer/trunk/test/translatortest/TestDebateInlineRef.java
@@ -0,0 +1,19 @@
+package translatortest;
+
+
+/***
+ * Testing debates
+ *
+ * 14 May 2010
+ * @author Ashok Hariharan
+ */
+public class TestDebateInlineRef extends OATranslatorTestDebateBase {
+	public TestDebateInlineRef(){
+		super();
+		setInputDocument("test/testdocs/test_debate_inline_ref.odt");
+		setOutputDocument("test/testresults/test_debate_inline_ref.xml");
+		setOutputMetalex("test/testresults/test_debate_inline_ref_mlx.xml");
+		setComparisonDocument("test/testdocs/debaterecord_test_comp.xml");
+	}
+	
+}
