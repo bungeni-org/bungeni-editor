72f70d08caba3524953b30475691b33c8c278ce8
smr.ayesh <smr.ayesh@gmail.com>
2012-05-31 11:55:38 +0000

    act

---
+++ utils/BungeniConnector/trunk/src/org/bungeni/connector/restlet/current/ActClassificationsRestlet.java
@@ -0,0 +1,61 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.bungeni.connector.restlet.current;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
+import java.util.List;
+import org.bungeni.connector.IBungeniConnector;
+import org.bungeni.connector.element.ActClassification;
+import org.restlet.Request;
+import org.restlet.Response;
+import org.restlet.Restlet;
+import org.restlet.data.MediaType;
+import org.restlet.data.Method;
+import org.restlet.data.Status;
+
+/**
+ *
+ * @author bzuadmin
+ */
+public class ActClassificationsRestlet extends Restlet {
+     static final String HEADER = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" + "\n<"+ActClassification.PACKAGE_ALIAS+">";
+    static final String FOOTER = "\n</"+ActClassification.PACKAGE_ALIAS+">";
+    IBungeniConnector bungeniConnector = null;
+
+    public ActClassificationsRestlet(IBungeniConnector bungeniConnector) {
+        this.bungeniConnector = bungeniConnector;
+    }
+
+    @Override
+    public void handle(Request request, Response response) {
+        try {
+            if (request.getMethod().equals(Method.GET)) {
+                List<ActClassification> items = bungeniConnector.getActClassifications();
+                response.setStatus(Status.SUCCESS_OK);
+
+                XStream xStream = new XStream(new DomDriver());
+                xStream.alias(ActClassification.PACKAGE_ALIAS, List.class);
+                xStream.alias(ActClassification.CLASS_ALIAS, ActClassification.class);
+            
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < items.size(); i++) {
+                    builder.append(xStream.toXML(items.get(i))).append("\n");
+                }
+                String xml = builder.toString();
+       
+                if (!xml.isEmpty()) {
+                    response.setEntity(HEADER + xml + FOOTER, MediaType.TEXT_XML);
+                } else {
+                }
+            } else {
+                response.setStatus(Status.CLIENT_ERROR_BAD_REQUEST);
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+    
+}
