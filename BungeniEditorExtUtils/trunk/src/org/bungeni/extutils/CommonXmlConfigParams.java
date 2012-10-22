9ff46a54723c6cf0040ad74da7fa0319dfa8ff13
ashok.hariharan <kohsah@users.noreply.github.com>
2012-10-22 13:54:01 +0000

    Added common xml config params class

---
+++ BungeniEditorExtUtils/trunk/src/org/bungeni/extutils/CommonXmlConfigParams.java
@@ -0,0 +1,14 @@
+
+
+package org.bungeni.extutils;
+
+/**
+ * Common XML static config parameters
+ * @author Ashok Hariharan
+ */
+public class CommonXmlConfigParams {
+
+    public static String SAX_PARSER_DRIVER = "org.apache.xerces.parsers.SAXParser";
+
+    public static final String ODF_URI_PREFIX = "uri:";
+}
