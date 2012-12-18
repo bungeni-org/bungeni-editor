6c3795254c823c2eaddc5c99c5ccaf945d6d7e23
ashok.hariharan <kohsah@users.noreply.github.com>
2012-12-18 14:46:28 +0000

    Configuration switching disabeld on main form - default config has to be set manual on config.xml ; Added BungeniServiceAccess class to interact wtih Bungeni

---
+++ BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/editor/input/BungeniServiceAccess.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2012 Africa i-Parliaments Action Plan
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package org.bungeni.editor.input;
+
+import java.util.List;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+/**
+ *
+ * @author Ashok Hariharan
+ */
+public class BungeniServiceAccess {
+    
+    private BungeniServiceAccess instance = null;
+    
+    private boolean loggedIn = false;
+    
+    private String user = "";
+    private String password = "";
+    
+    
+    
+    public BungeniServiceAccess getInstance(){
+        if (null == instance) {
+            instance = new BungeniServiceAccess();
+        }
+        return instance;
+    }
+   
+    public DefaultHttpClient login(String appServer, String appBase, String user, String password) {
+        return null;
+    }
+   
+    public List searchDocuments(String searchServer, String docType, String status) {
+        return null;
+    }
+    
+    public boolean authenticateDocument(String urlDocument) {
+        return true;
+    }
+    
+    public List getDocumentAttachments(String urlDocument) {
+        return null;
+    }
+    
+}
