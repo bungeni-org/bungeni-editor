591824e00cfe05194bd22f952946b3d2b84ae70a
ashok.hariharan <kohsah@users.noreply.github.com>
2013-01-09 14:27:52 +0000

    Implementation of inline reference dialog setter for Bir Zeit testing

---
+++ BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/editor/selectors/act/Main.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2013 Africa i-Parliaments
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package org.bungeni.editor.selectors.act;
+
+import java.awt.Component;
+import org.bungeni.editor.selectors.BaseMetadataContainerPanel;
+
+/**
+ *
+ * @author Ashok Hariharan
+ */
+public class Main extends BaseMetadataContainerPanel{
+    
+      private static org.apache.log4j.Logger log             
+              = org.apache.log4j.Logger.getLogger(Main.class.getName());
+   
+    public Main() {
+        super();
+    }
+
+    @Override
+    public Component getPanelComponent() {
+        return this;
+    }
+
+    
+}
