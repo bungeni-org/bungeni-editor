cba0212d692becf4d01daff9218e198a40565adf
ashok.hariharan <kohsah@users.noreply.github.com>
2013-01-14 09:19:45 +0000

    Cleanup of imports in FrameLauncher

--- BungeniEditorExtUtils/trunk/src/org/bungeni/extutils/FrameLauncher.java
+++ BungeniEditorExtUtils/trunk/src/org/bungeni/extutils/FrameLauncher.java
@@ -1,8 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package org.bungeni.extutils;
 
 import java.awt.Component;
@@ -10,7 +5,6 @@ import java.awt.Dimension;
 import java.awt.Toolkit;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
-import javax.swing.JRootPane;
 
 /**
  *
