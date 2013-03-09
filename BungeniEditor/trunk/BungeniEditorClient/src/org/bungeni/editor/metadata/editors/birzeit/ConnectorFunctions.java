24433536e0f4d1016ceec13a493f185c890abc73
smr.ayesh@gmail.com <smr.ayesh@gmail.com>
2013-03-09 07:39:21 +0000

    editor/birzeit

--- BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/editor/metadata/editors/birzeit/ConnectorFunctions.java
+++ BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/editor/metadata/editors/birzeit/ConnectorFunctions.java
@@ -17,39 +17,67 @@
  */
 package org.bungeni.editor.metadata.editors.birzeit;
 
+import com.mysql.jdbc.Field;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Properties;
+import org.bungeni.connector.ConnectorProperties;
+import org.bungeni.connector.client.BungeniConnector;
+import org.bungeni.connector.server.DataSourceServer;
+import org.bungeni.extutils.CommonDataSourceFunctions;
 
 /**
+ * Common library for interfacing with BungeniConnector
  *
- * @author PC
+ * @author Reagan
  */
 public class ConnectorFunctions {
-        public static ResultSet ConnectMMSM(String dbName, String sqlStm) {
-        String connectionUrl = "jdbc:sqlserver://l-share;databaseName="+ dbName+ ";selectMethod=cursor";
 
+    public static DataSourceServer startDSServer() throws IOException {
+        DataSourceServer dss = DataSourceServer.getInstance();
+        Properties dsProps = CommonDataSourceFunctions.getDataSourceProperties();
+        dss.loadProperties(dsProps);
+        dss.startServer();
+        return dss;
+    }
+
+    // !+BUNGENI_CONNECTOR(Ashok ,05-01-2012)
+    // This method changes the way classes acting as BungeniConnector
+    // clients initialise themselves to ensure that they load
+    //  the properties using the REST API rather than directly
+    public static BungeniConnector getDSClient() throws IOException {
+        BungeniConnector client = new BungeniConnector();
+        client.init(new ConnectorProperties(CommonDataSourceFunctions.getDataSourceProperties()));
+        return client;
+    }
+
+    public static Connection ConnectMMSM(String dbName) {
+         String connectionUrl = "jdbc:sqlserver://l-share;databaseName=" + dbName + ";selectMethod=cursor;CharacterSet=UTF-8";
+
+ //       String connectionUrl = "jdbc:sqlserver://BZUADMIN-HP;instance=SQLEXPRESS;databaseName=Muqtafi_test;selectMethod=cursor;CharacterSet=UTF-8";
         // Declare the JDBC objects.
         Connection con = null;
-        Statement stmt = null;
-        ResultSet rs = null;
+
+        //   ResultSet rs = null;
 
         try {
             // Establish the connection.
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-
+//            System.setProperty("java.library.path", "C:\\Users\\bzuadmin\\Desktop\\BungeniClientEditor\\BungeniEditorClient\\lib\\Microsoft JDBC Driver 4.0 for SQL Server\\sqljdbc_4.0\\enu\\auth\\x86\\sqljdbc_auth.dll");
+//
+//            java.lang.reflect.Field fieldSysPath = ClassLoader.class.getDeclaredField("sys_paths");
+//            fieldSysPath.setAccessible(true);
+//            fieldSysPath.set(null, null);
+            //   con = DriverManager.getConnection(connectionUrl, "sayesh", "QWE!@#qwe"); 
             con = DriverManager.getConnection(connectionUrl, "sayesh", "QWE!@#qwe");
-
-            // Create and execute an SQL statement that returns some data.
-            stmt = con.createStatement();
-            rs = stmt.executeQuery(sqlStm);
         } // Handle any errors that may have occurred.
         catch (Exception e) {
             e.printStackTrace();
         } finally {
-            return rs;
+            return con;
         }
     }
-
 }
