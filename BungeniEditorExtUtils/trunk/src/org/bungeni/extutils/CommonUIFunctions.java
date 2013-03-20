0c46e1bf8c333400f934ec0befeb3d0288570b2a
ashok.hariharan <kohsah@users.noreply.github.com>
2013-03-20 14:25:45 +0000

    Adding disablePanel api to ext-utils

--- BungeniEditorExtUtils/trunk/src/org/bungeni/extutils/CommonUIFunctions.java
+++ BungeniEditorExtUtils/trunk/src/org/bungeni/extutils/CommonUIFunctions.java
@@ -3,15 +3,16 @@ package org.bungeni.extutils;
 
 //~--- JDK imports ------------------------------------------------------------
 
-import java.awt.Color;
 import java.awt.Component;
 import java.awt.ComponentOrientation;
 import java.awt.Container;
-
 import java.util.ArrayList;
 import java.util.Locale;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JRootPane;
+import javax.swing.SwingUtilities;
 
 /**
  *
@@ -20,8 +21,6 @@ import javax.swing.JRootPane;
  * @author Ashok Hariharan
  */
 public class CommonUIFunctions {
-
-
 
     public static ArrayList<String> findComponentsWithNames(Container container) {
         ArrayList<String> fieldsWithNames = new ArrayList<String>(0);
@@ -72,25 +71,41 @@ public class CommonUIFunctions {
         return null;
     }
 
+/**
+ * Changes the Orientation of the components depending on the Locale
+ *
+ * @param comp Component handle
+ */
+ public static void compOrientation(Component comp)
+ {
+   //!+ORIENTATION(ah, 03-05-2012) - original version by smr.ayesh was appling orientation
+   //as Container, added a fallback if it isnt a container, to use the regular Component class
+   //apply orientation api
+   //!+ORIENTATION(ah, 04-05-2012) - moved to CommonUIFunctions 
+   if (comp instanceof Container) {
+        ((Container)comp).applyComponentOrientation(ComponentOrientation.getOrientation(Locale.getDefault()));
+   } else {
+        comp.applyComponentOrientation(ComponentOrientation.getOrientation(Locale.getDefault()));
+   }
 
+ }
 
-    /**
-     * Changes the Orientation of the components depending on the Locale
-     *
-     * @param comp Component handle
-     */
-     public static void compOrientation(Component comp)
-     {
-       //!+ORIENTATION(ah, 03-05-2012) - original version by smr.ayesh was appling orientation
-       //as Container, added a fallback if it isnt a container, to use the regular Component class
-       //apply orientation api
-       //!+ORIENTATION(ah, 04-05-2012) - moved to CommonUIFunctions 
-       if (comp instanceof Container) {
-            ((Container)comp).applyComponentOrientation(ComponentOrientation.getOrientation(Locale.getDefault()));
-       } else {
-            comp.applyComponentOrientation(ComponentOrientation.getOrientation(Locale.getDefault()));
-       }
+  /**
+   * Requires the use of DisabledGlassPane 
+   * @param glassPane
+   * @param parentDialog
+   * @param message 
+   */
+  public void disablePanel(DisabledGlassPane glassPane, JDialog parentDialog, String message){
+        JRootPane rootPane = SwingUtilities.getRootPane(parentDialog);
+        rootPane.setGlassPane(glassPane);
+        glassPane.activate(message);
+  }
 
-     }
+  public void disablePanel(DisabledGlassPane glassPane, JFrame parentDialog, String message){
+        JRootPane rootPane = SwingUtilities.getRootPane(parentDialog);
+        rootPane.setGlassPane(glassPane);
+        glassPane.activate(message);
+  }
 
 }
