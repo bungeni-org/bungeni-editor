f7876c5b79f7dcec9d1a48b0fbafe1ef9827b1f0
ashok.hariharan <kohsah@users.noreply.github.com>
2013-06-11 09:33:33 +0000

    Adding GSON serializer classes for doc and vocabulary

---
+++ BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/ext/integration/bungeniportal/docimpl/BungeniDoc.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright (C) 2013 UN/DESA Africa i-Parliaments Action Plan
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package org.bungeni.ext.integration.bungeniportal.docimpl;
+
+/**
+ * GSON Mapping class for Bungeni Doc types
+ * @author Ashok Hariharan
+ */
+public class BungeniDoc {
+    private String admissible_date;
+    private String body;
+    private Integer doc_id;
+    private String doc_type ; 
+    
+    private BungeniVocabType language;
+    
+    private String notice_date;
+    private Integer owner_id;
+    
+    private BungeniVocabType status;
+    private String status_date;
+    private String submission_date;
+    private String timestamp;
+    private String title;
+    private String type;
+
+    /**
+     * @return the admissible_date
+     */
+    public String getAdmissibleDate() {
+        return admissible_date;
+    }
+
+    /**
+     * @return the body
+     */
+    public String getBody() {
+        return body;
+    }
+
+    /**
+     * @return the doc_id
+     */
+    public Integer getDocId() {
+        return doc_id;
+    }
+
+    /**
+     * @return the doc_type
+     */
+    public String getDocType() {
+        return doc_type;
+    }
+
+    /**
+     * @return the language
+     */
+    public BungeniVocabType getLanguage() {
+        return language;
+    }
+
+    /**
+     * @return the notice_date
+     */
+    public String getNoticeDate() {
+        return notice_date;
+    }
+
+    /**
+     * @return the owner_id
+     */
+    public Integer getOwnerId() {
+        return owner_id;
+    }
+
+    /**
+     * @return the status
+     */
+    public BungeniVocabType getStatus() {
+        return status;
+    }
+
+    /**
+     * @return the status_date
+     */
+    public String getStatusDate() {
+        return status_date;
+    }
+
+    /**
+     * @return the submission_date
+     */
+    public String getSubmissionDate() {
+        return submission_date;
+    }
+
+    /**
+     * @return the timestamp
+     */
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    /**
+     * @return the title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * @return the type
+     */
+    public String getType() {
+        return type;
+    }
+    
+
+}
---
+++ BungeniEditor/trunk/BungeniEditorClient/src/org/bungeni/ext/integration/bungeniportal/docimpl/BungeniVocabType.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2013 UN/DESA Africa i-Parliaments Action Plan
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package org.bungeni.ext.integration.bungeniportal.docimpl;
+
+/**
+ * GSON Serializer class for vocabulary types
+ * @author Ashok Hariharan
+ */
+public class BungeniVocabType {
+    private String displayAs ;
+    private String name;
+    private String value;
+
+    /**
+     * @return the displayAs
+     */
+    public String getDisplayAs() {
+        return displayAs;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @return the value
+     */
+    public String getValue() {
+        return value;
+    }
+    
+   
+    
+}
